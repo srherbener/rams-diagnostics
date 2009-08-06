52883a62c6966083ece20a6f95bf2a225e5b91e3
sherbener <sherbener>
2009-08-06 04:25:59 +0000

    added supercooled liquid capability (triggered when var is 'liquid_s')

--- colint.f90
+++ colint.f90
@@ -43,19 +43,26 @@ program main
   ! The *Loc vars hold the locations of w, press, var in the GRADS
   ! data files: the first index is the file number, the second index is the
   ! var number
-  real, dimension(:,:,:,:), allocatable :: Dens, ColVar, ColInt
+  real, dimension(:,:,:,:), allocatable :: Dens, TempC, ColVar, ColInt
   real, dimension(:), allocatable :: ZmHeights
   real, dimension(1:1) :: DummyZcoords
   real :: Xstart, Xinc, Ystart, Yinc
-  type (GradsVarLocation) :: DensLoc, VarLoc
+  type (GradsVarLocation) :: DensLoc, TempLoc, VarLoc
 
   integer :: i
   integer :: Ierror
 
   integer :: ix, iy, iz, it
+  logical :: SuperCooled
 
   ! Get the command line argument
   call GetMyArgs(Infiles, OfileBase, VarToInt)
+  if (VarToInt .eq. 'liquid_s') then
+    SuperCooled = .true.
+    VarToInt = 'liquid'
+  else
+    SuperCooled = .false.
+  end if
   call String2List(Infiles, ':', GradsCtlFiles, MaxFiles, Nfiles, 'input files')
 
   write (*,*) 'Calculating column integration for RAMS data:'
@@ -65,6 +72,9 @@ program main
   end do
   write (*,*) '  Output file base name:  ', trim(OfileBase)
   write (*,*) '  RAMS variable that is being integrated: ', trim(VarToInt)
+  if (SuperCooled) then
+    write (*,*) '    Applying tempc filter for supercooled liquid' 
+  end if
   write (*,*) ''
 
 
@@ -75,7 +85,7 @@ program main
   end do
   write (*,*) ''
 
-  call CheckDataDescrip_CI(GdataDescrip, Nfiles, Nx, Ny, Nz, Nt, Nvars, DensLoc, VarLoc, VarToInt)
+  call CheckDataDescrip_CI(GdataDescrip, Nfiles, Nx, Ny, Nz, Nt, Nvars, DensLoc, TempLoc, VarLoc, VarToInt)
 
   write (*,*) 'Gridded data information:'
   write (*,*) '  Number of x (longitude) points:          ', Nx
@@ -89,13 +99,15 @@ program main
 
   write (*,*) 'Locations of variables in GRADS data (file number, var number):'
   write (*,'(a20,i3,a2,i3,a1)') 'dn0: (', DensLoc%Fnum, ', ', DensLoc%Vnum, ')'
+  write (*,'(a20,i3,a2,i3,a1)') 'tempc: (', TempLoc%Fnum, ', ', TempLoc%Vnum, ')'
   write (*,'(a17,a3,i3,a2,i3,a1)') trim(VarToInt), ': (', VarLoc%Fnum, ', ', VarLoc%Vnum, ')'
   write (*,*) ''
 
   ! Allocate the data arrays and read in the data from the GRADS data files
   ! Make sure ZmHeights goes from 0 to n
-  allocate (Dens(1:Nx,1:Ny,1:Nz,1:Nt), ColVar(1:Nx,1:Ny,1:Nz,1:Nt), &
-            ZmHeights(0:Nz), ColInt(1:Nx, 1:Ny, 1:1, 1:Nt), stat=Ierror)
+  allocate (Dens(1:Nx,1:Ny,1:Nz,1:Nt), TempC(1:Nx,1:Ny,1:Nz,1:Nt), &
+            ColVar(1:Nx,1:Ny,1:Nz,1:Nt), ZmHeights(0:Nz), &
+            ColInt(1:Nx, 1:Ny, 1:1, 1:Nt), stat=Ierror)
   if (Ierror .ne. 0) then
     write (*,*) 'ERROR: Data array memory allocation failed'
     stop
@@ -103,6 +115,7 @@ program main
 
   ! Read in the data for the vars using the description and location information
   call ReadGradsData(GdataDescrip, 'dn0', DensLoc, Dens, Nx, Ny, Nz, Nt)
+  call ReadGradsData(GdataDescrip, 'tempc', TempLoc, TempC, Nx, Ny, Nz, Nt)
   call ReadGradsData(GdataDescrip, VarToInt, VarLoc, ColVar, Nx, Ny, Nz, Nt)
 
   ! Set the z heights which can be used to determine the layer thicknesses
@@ -127,8 +140,15 @@ program main
       do iy = 1, Ny
         ColInt(ix,iy,1,it) = 0.0
         do iz = 1, Nz
-          ColInt(ix,iy,1,it) = ColInt(ix,iy,1,it) + &
+          if (SuperCooled) then
+            if (TempC(ix,iy,iz,it) .le. 0.0) then
+              ColInt(ix,iy,1,it) = ColInt(ix,iy,1,it) + &
+                (ColVar(ix,iy,iz,it) * Dens(ix,iy,iz,it) * (ZmHeights(iz) - ZmHeights(iz-1)))
+            end if
+          else
+            ColInt(ix,iy,1,it) = ColInt(ix,iy,1,it) + &
               (ColVar(ix,iy,iz,it) * Dens(ix,iy,iz,it) * (ZmHeights(iz) - ZmHeights(iz-1)))
+          end if
         enddo
       enddo
     enddo
@@ -146,7 +166,7 @@ program main
   call WriteGrads(GoutDescrip, ColInt)
 
   ! Clean up
-  deallocate (Dens, ColVar, ZmHeights, ColInt, stat=Ierror)
+  deallocate (Dens, TempC, ColVar, ZmHeights, ColInt, stat=Ierror)
   if (Ierror .ne. 0) then
     write (*,*) 'ERROR: Data array memory de-allocation failed'
     stop
