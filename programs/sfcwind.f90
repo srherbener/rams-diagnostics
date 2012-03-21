87bd86680dec60f7d529090d5efd2cd172082dfe
steve <sherbener@atmos.colostate.edu>
2012-03-21 10:35:54 -0600

    Ported sfcwind to new GRADS var scheme

--- programs/sfcwind.f90
+++ programs/sfcwind.f90
@@ -27,18 +27,14 @@ program main
   integer, parameter :: LargeString=512
   integer, parameter :: MediumString=256
   integer, parameter :: LittleString=128
-  integer, parameter :: MaxFiles=10
 
   character (len=LargeString) :: Infiles
   character (len=MediumString) :: OfileBase
   character (len=LittleString) :: VarToInt
 
-  character (len=MediumString), dimension(1:MaxFiles) :: GradsCtlFiles
-  integer :: Nfiles
+  type (GradsControlFiles):: GctlFiles
 
-  type (GradsDataDescription), dimension(1:MaxFiles) :: GdataDescrip
-  integer :: Nx, Ny, Nz, Nt, Nvars
-  type (GradsOutDescription) :: GoutDescrip
+  integer :: Nx, Ny, Nz, Nt
 
   ! Data arrays: need one for dn0 (density) and the var we are doing the
   ! column integration on
@@ -46,93 +42,75 @@ program main
   ! The *Loc vars hold the locations of w, press, var in the GRADS
   ! data files: the first index is the file number, the second index is the
   ! var number
-  real, dimension(:,:,:,:), allocatable :: U, V, SfcWind
+  type (GradsVar) :: U, V, SfcWind
   real, dimension(1:1) :: DummyZcoords
   real :: Xstart, Xinc, Ystart, Yinc
-  type (GradsVarLocation) :: Uloc, Vloc
 
   integer :: i
-  integer :: Ierror
 
   integer :: ix, iy, iz, it
 
   ! Get the command line argument
   call GetMyArgs(Infiles, OfileBase)
-  call String2List(Infiles, ':', GradsCtlFiles, MaxFiles, Nfiles, 'input files')
+  call String2List(Infiles, ':', GctlFiles%Fnames, MaxFiles, GctlFiles%Nfiles, 'input files')
 
   write (*,*) 'Calculating magnitude of surface wind for RAMS data:'
   write (*,*) '  GRADS input control files:'
-  do i = 1, Nfiles
-    write (*,*) '  ', i, ': ', trim(GradsCtlFiles(i))
+  do i = 1, GctlFiles%Nfiles
+    write (*,*) '  ', i, ': ', trim(GctlFiles%Fnames(i))
   end do
   write (*,*) '  Output file base name:  ', trim(OfileBase)
   write (*,*) ''
 
 
   ! Read the GRADS data description files and collect the information about the data
-  do i = 1, Nfiles
-    write (*,*) 'Reading GRADS Control File: ', trim(GradsCtlFiles(i))
-    call ReadGradsCtlFile(GradsCtlFiles(i), GdataDescrip(i))
-  end do
-  write (*,*) ''
+  call ReadGradsCtlFiles(GctlFiles)
 
-  call CheckDataDescrip_SW(GdataDescrip, Nfiles, Nx, Ny, Nz, Nt, Nvars, Uloc, Vloc)
+  call InitGvarFromGdescrip(GctlFiles, U, 'u')
+  call InitGvarFromGdescrip(GctlFiles, V, 'v')
 
-  write (*,*) 'Gridded data information:'
-  write (*,*) '  Number of x (longitude) points:          ', Nx
-  write (*,*) '  Number of y (latitude) points:           ', Ny
-  write (*,*) '  Number of z (vertical level) points:     ', Nz
-  write (*,*) '  Number of t (time) points:               ', Nt
-  write (*,*) '  Total number of grid variables:          ', Nvars
-  write (*,*) ''
-  write (*,*) '  Number of data values per grid variable: ', Nx*Ny*Nz*Nt
-  write (*,*) ''
-
-  write (*,*) 'Locations of variables in GRADS data (file number, var number):'
-  write (*,'(a20,i3,a2,i3,a1)') 'u: (', Uloc%Fnum, ', ', Uloc%Vnum, ')'
-  write (*,'(a20,i3,a2,i3,a1)') 'v: (', Vloc%Fnum, ', ', Vloc%Vnum, ')'
-  write (*,*) ''
-
-  ! Allocate the data arrays and read in the data from the GRADS data files
-  allocate (U(1:Nx,1:Ny,1:Nz,1:Nt), V(1:Nx,1:Ny,1:Nz,1:Nt), &
-            SfcWind(1:Nx, 1:Ny, 1:1, 1:Nt), stat=Ierror)
-  if (Ierror .ne. 0) then
-    write (*,*) 'ERROR: Data array memory allocation failed'
+  if (.not. (GvarDimsMatch(U, V, .false.))) then
+    write (*,*) 'ERROR: dimensions of u, and v do not match'
     stop
-  end if
+  endif
+
+  ! U and V have matching dimensions if we got to here, so use U as a representative
+  write (*,*) 'Gridded data information:'
+  write (*,*) '  Number of x (longitude) points:          ', U%Nx
+  write (*,*) '  Number of y (latitude) points:           ', U%Ny
+  write (*,*) '  Number of z (vertical level) points:     ', U%Nz
+  write (*,*) '  Number of t (time) points:               ', U%Nt
+  write (*,*) ''
+  write (*,*) '  Number of data values per grid variable: ', U%Nx*U%Ny*U%Nz*U%Nt
+  write (*,*) ''
+
+  write (*,*) 'Locations of variables in GRADS data (file, var number):'
+  write (*,'(a20,a,a2,i3,a1)') 'u: (', trim(U%DataFile), ', ', U%Vnum, ')'
+  write (*,'(a20,a,a2,i3,a1)') 'v: (', trim(V%DataFile), ', ', V%Vnum, ')'
+  write (*,*) ''
 
   ! Read in the data for the vars using the description and location information
-  call ReadGradsData(GdataDescrip, 'u', Uloc, U, Nx, Ny, Nz, Nt)
-  call ReadGradsData(GdataDescrip, 'v', Vloc, V, Nx, Ny, Nz, Nt)
+  call ReadGradsData(U)
+  call ReadGradsData(V)
+
+  DummyZcoords(1) = 0.0
+  call InitGradsVar(SfcWind, 'sfcwind', U%Nx, U%Ny, 1, U%Nt, &
+                    U%Xstart, U%Xinc, U%Ystart, U%Yinc, DummyZcoords, U%Tstart, U%Tinc, &
+                    U%UndefVal, '<NONE>', 0, 0)
+
 
   ! Generate a surface wind magnitude each time step
   !   mag = sqrt (u**2 + v**2)
   iz = 2 ! use second level to help avoid damping from surface friction
-  do it = 1, Nt
-    do ix = 1, Nx
-      do iy = 1, Ny
-        SfcWind(ix,iy,1,it) = sqrt(U(ix,iy,iz,it)**2 + V(ix,iy,iz,it)**2)
+  do it = 1, U%Nt
+    do ix = 1, U%Nx
+      do iy = 1, U%Ny
+        SfcWind%Vdata(it,1,ix,iy) = sqrt(U%Vdata(it,iz,ix,iy)**2 + V%Vdata(it,iz,ix,iy)**2)
       enddo
     enddo
   enddo
 
-  DummyZcoords(1) = 0.0
-  Xstart = GdataDescrip(1)%Xcoords(1)
-  Xinc = GdataDescrip(1)%Xcoords(2) - Xstart
-  Ystart = GdataDescrip(1)%Ycoords(1)
-  Yinc = GdataDescrip(1)%Ycoords(2) - Ystart
-  call BuildGoutDescrip(Nx, Ny, 1, Nt, SfcWind, OfileBase, GdataDescrip(1)%UndefVal, &
-          'sfcwind', Xstart, Xinc, Ystart, Yinc, DummyZcoords, &
-          GdataDescrip(1)%Tstart, GdataDescrip(1)%Tinc, GoutDescrip, 'sfc_wind')
-
-  call WriteGrads(GoutDescrip, SfcWind)
-
-  ! Clean up
-  deallocate (U, V, SfcWind, stat=Ierror)
-  if (Ierror .ne. 0) then
-    write (*,*) 'ERROR: Data array memory de-allocation failed'
-    stop
-  end if
+  call WriteGrads(SfcWind, OfileBase, 'sfc_wind')
 
   stop
 end
