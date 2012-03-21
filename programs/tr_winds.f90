818e0dabc4f7a3059a5a04ab00b081c33954a67a
steve <sherbener@atmos.colostate.edu>
2012-03-21 11:12:26 -0600

    Ported tr_winds to the new GRADS var scheme

--- programs/tr_winds.f90
+++ programs/tr_winds.f90
@@ -26,18 +26,12 @@ program main
   integer, parameter :: LargeString=512
   integer, parameter :: MediumString=256
   integer, parameter :: LittleString=128
-  integer, parameter :: MaxFiles=10
 
   character (len=LargeString) :: Infiles
   character (len=MediumString) :: OfileBaseTan, OfileBaseRad
   character (len=LittleString) :: VarName
 
-  character (len=MediumString), dimension(1:MaxFiles) :: GradsCtlFiles
-  integer :: Nfiles
-
-  type (GradsDataDescription), dimension(1:MaxFiles) :: GdataDescrip
-  integer :: Nx, Ny, Nz, Nt, Nvars
-  type (GradsOutDescription) :: GoutDescrip
+  type (GradsControlFiles) :: GctlFiles
 
   ! Data arrays: need one for w (vertical velocity), press (pressure)
   ! and the var we are doing the averaging on
@@ -45,13 +39,11 @@ program main
   ! The *Loc vars hold the locations of w, press, var in the GRADS
   ! data files: the first index is the file number, the second index is the
   ! var number
-  real, dimension(:,:,:,:), allocatable :: U, V, Press, Rad, Tang
+  type (GradsVar) :: U, V, Press, Rad, Tang
   integer, dimension(:), allocatable :: StmIx, StmIy
   real, dimension(:), allocatable :: MinP, Xcoords, Ycoords
-  type (GradsVarLocation) :: Ploc, Uloc, Vloc
 
   integer :: i
-  integer :: Ierror
 
   integer :: ix, iy, iz, it
 
@@ -59,96 +51,75 @@ program main
 
   ! Get the command line arguments
   call GetMyArgs(Infiles, OfileBaseTan, OfileBaseRad)
-  call String2List(Infiles, ':', GradsCtlFiles, MaxFiles, Nfiles, 'input files')
+  call String2List(Infiles, ':', GctlFiles%Fnames, MaxFiles, GctlFiles%Nfiles, 'input files')
 
   write (*,*) 'Calculating storm tangential and radial winds for RAMS data:'
   write (*,*) '  GRADS input control files:'
-  do i = 1, Nfiles
-    write (*,*) '  ', i, ': ', trim(GradsCtlFiles(i))
+  do i = 1, GctlFiles%Nfiles
+    write (*,*) '  ', i, ': ', trim(GctlFiles%Fnames(i))
   end do
   write (*,*) '  Output file base name, (tangential winds):  ', trim(OfileBaseTan)
   write (*,*) '  Output file base name, (radial winds):  ', trim(OfileBaseRad)
   write (*,*) ''
 
   ! Read the GRADS data description files and collect the information about the data
-  do i = 1, Nfiles
-    write (*,*) 'Reading GRADS Control File: ', trim(GradsCtlFiles(i))
-    call ReadGradsCtlFile(GradsCtlFiles(i), GdataDescrip(i))
-  end do
-  write (*,*) ''
+  call ReadGradsCtlFiles(GctlFiles)
 
-  call CheckDataDescrip_TR(GdataDescrip, Nfiles, Nx, Ny, Nz, Nt, Nvars, Ploc, Uloc, Vloc)
+  call InitGvarFromGdescrip(GctlFiles, U, 'u')
+  call InitGvarFromGdescrip(GctlFiles, V, 'v')
+  call InitGvarFromGdescrip(GctlFiles, Press, 'press')
 
-  write (*,*) 'Gridded data information:'
-  write (*,*) '  Number of x (longitude) points:          ', Nx
-  write (*,*) '  Number of y (latitude) points:           ', Ny
-  write (*,*) '  Number of z (vertical level) points:     ', Nz
-  write (*,*) '  Number of t (time) points:               ', Nt
-  write (*,*) '  Total number of grid variables:          ', Nvars
-  write (*,*) ''
-  write (*,*) '  Number of data values per grid variable: ', Nx*Ny*Nz*Nt
-  write (*,*) ''
-
-  write (*,*) 'Locations of variables in GRADS data (file number, var number):'
-  write (*,'(a20,i3,a2,i3,a1)') 'pressure: (', Ploc%Fnum, ', ', Ploc%Vnum, ')'
-  write (*,'(a20,i3,a2,i3,a1)') 'speed - u: (', Uloc%Fnum, ', ', Uloc%Vnum, ')'
-  write (*,'(a20,i3,a2,i3,a1)') 'speed - v: (', Vloc%Fnum, ', ', Vloc%Vnum, ')'
-  write (*,*) ''
-
-  ! Allocate the data arrays and read in the data from the GRADS data files
-  allocate (U(1:Nx,1:Ny,1:Nz,1:Nt), V(1:Nx,1:Ny,1:Nz,1:Nt), Press(1:Nx,1:Ny,1:Nz,1:Nt), &
-            Rad(1:Nx,1:Ny,1:Nz,1:Nt), Tang(1:Nx,1:Ny,1:Nz,1:Nt), &
-            StmIx(1:Nt), StmIy(1:Nt), MinP(1:Nt), Xcoords(1:Nx), Ycoords(1:Ny), stat=Ierror)
-  if (Ierror .ne. 0) then
-    write (*,*) 'ERROR: Data array memory allocation failed'
+  if (.not. (GvarDimsMatch(U, V, .false.) .and. GvarDimsMatch(U, Press, .false.))) then
+    write (*,*) 'ERROR: dimensions of u, v and press do not match'
     stop
-  end if
+  endif
+
+  ! At this point the dimensions of U, V, Press match so use U as a representative
+  write (*,*) 'Gridded data information:'
+  write (*,*) '  Number of x (longitude) points:          ', U%Nx
+  write (*,*) '  Number of y (latitude) points:           ', U%Ny
+  write (*,*) '  Number of z (vertical level) points:     ', U%Nz
+  write (*,*) '  Number of t (time) points:               ', U%Nt
+  write (*,*) ''
+  write (*,*) '  Number of data values per grid variable: ', U%Nx*U%Ny*U%Nz*U%Nt
+  write (*,*) ''
+
+  write (*,*) 'Locations of variables in GRADS data (file, var number):'
+  write (*,'(a20,a,a2,i3,a1)') 'speed - u: (', trim(U%DataFile), ', ', U%Vnum, ')'
+  write (*,'(a20,a,a2,i3,a1)') 'speed - v: (', trim(V%DataFile), ', ', V%Vnum, ')'
+  write (*,'(a20,a,a2,i3,a1)') 'press: (', trim(Press%DataFile), ', ', Press%Vnum, ')'
+  write (*,*) ''
 
   ! Convert the GRADS grid coordinates from longitude, latitude to flat plane (x and y).
-  call ConvertGridCoords(Nx, Ny, GdataDescrip(1), Xcoords, Ycoords)
+  call ConvertGridCoords(U, Xcoords, Ycoords)
 
   write (*,*) 'Horzontal Grid Coordinate Info:'
   write (*,*) '  X Range (min lon, max lon) --> (min x, max x): '
-  write (*,*) '    ', GdataDescrip(1)%Xcoords(1), GdataDescrip(1)%Xcoords(Nx), Xcoords(1), Xcoords(Nx)
+  write (*,*) '    ', U%Xcoords(1), U%Xcoords(U%Nx), Xcoords(1), Xcoords(U%Nx)
   write (*,*) '  Y Range: '
-  write (*,*) '    ', GdataDescrip(1)%Ycoords(1), GdataDescrip(1)%Ycoords(Ny), Ycoords(1), Ycoords(Ny)
+  write (*,*) '    ', U%Ycoords(1), U%Ycoords(U%Ny), Ycoords(1), Ycoords(U%Ny)
   write (*,*) ''
 
+  ! initialize the output variables
+  call InitGradsVar(Tang, 'wind_speed' , U%Nx, U%Ny, U%Nz, U%Nt, &
+                    U%Xstart, U%Xinc, U%Ystart, U%Yinc, U%Zcoords, U%Tstart, U%Tinc, &
+                    U%UndefVal, '<NONE>', 0, 0)
+  call InitGradsVar(Rad, 'wind_speed' , U%Nx, U%Ny, U%Nz, U%Nt, &
+                    U%Xstart, U%Xinc, U%Ystart, U%Yinc, U%Zcoords, U%Tstart, U%Tinc, &
+                    U%UndefVal, '<NONE>', 0, 0)
+
+
   ! Read in the data for the vars using the description and location information
-  call ReadGradsData(GdataDescrip, 'press', Ploc, Press, Nx, Ny, Nz, Nt)
-  call RecordStormCenter(Nx, Ny, Nz, Nt, Press, StmIx, StmIy, MinP)
-  call ReadGradsData(GdataDescrip, 'u', Uloc, U, Nx, Ny, Nz, Nt)
-  call ReadGradsData(GdataDescrip, 'v', Vloc, V, Nx, Ny, Nz, Nt)
-  call ConvertHorizVelocity(Nx, Ny, Nz, Nt, U, V, StmIx, StmIy, &
-                  Xcoords, Ycoords, Tang, .true.)
-  call ConvertHorizVelocity(Nx, Ny, Nz, Nt, U, V, StmIx, StmIy, &
-                  Xcoords, Ycoords, Rad, .false.)
+  call ReadGradsData(U)
+  call ReadGradsData(V)
+  call ReadGradsData(Press)
 
-  ! Want lat, lon values so grab them from the description arrays
-  Xstart = GdataDescrip(1)%Xcoords(1)
-  Xinc = GdataDescrip(1)%Xcoords(2) - GdataDescrip(1)%Xcoords(1)
-  Ystart = GdataDescrip(1)%Ycoords(1)
-  Yinc = GdataDescrip(1)%Ycoords(2) - GdataDescrip(1)%Ycoords(1)
-  VarName = 'wind_speed'
+  call RecordStormCenter(Press, StmIx, StmIy, MinP)
+  call ConvertHorizVelocity(U, V, StmIx, StmIy, Xcoords, Ycoords, Tang, .true.)
+  call ConvertHorizVelocity(U, V, StmIx, StmIy, Xcoords, Ycoords, Rad, .false.)
 
-  ! Write the tangential wind values
-  call BuildGoutDescrip(Nx, Ny, Nz, Nt, Tang, OfileBaseTan, GdataDescrip(1)%UndefVal, VarName, &
-          Xstart, Xinc, Ystart, Yinc, GdataDescrip(1)%Zcoords, GdataDescrip(1)%Tstart, &
-          GdataDescrip(1)%Tinc, GoutDescrip, 'tan_winds')
-  call WriteGrads(GoutDescrip, Tang)
-
-  ! Write the radial wind values
-  call BuildGoutDescrip(Nx, Ny, Nz, Nt, Rad, OfileBaseRad, GdataDescrip(1)%UndefVal, VarName, &
-          Xstart, Xinc, Ystart, Yinc, GdataDescrip(1)%Zcoords, GdataDescrip(1)%Tstart, &
-          GdataDescrip(1)%Tinc, GoutDescrip, 'rad_winds')
-  call WriteGrads(GoutDescrip, Rad)
-
-  ! Clean up
-  deallocate (U, V, Press, Tang, Rad, StmIx, StmIy, MinP, stat=Ierror)
-  if (Ierror .ne. 0) then
-    write (*,*) 'ERROR: Data array memory de-allocation failed'
-    stop
-  end if
+  call WriteGrads(Tang, OfileBaseTan, 'tan_winds')
+  call WriteGrads(Rad,  OfileBaseRad, 'rad_winds')
 
   stop
 end
