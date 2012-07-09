2e4c5b8ace4044d1f9136aafaad9f4624e51eab4
steve <sherbener@atmos.colostate.edu>
2012-07-09 13:07:52 -0600

    Added the copying of coordinate data from the input files so that the output (joined) file can be used by GRADS.

--- src/join_hdata.f90
+++ src/join_hdata.f90
@@ -32,9 +32,14 @@ program join_hdata
   ! OutVar dims: x, y, z, t
   ! Invars, array where each element has a data array with dims: x, y, z, t
   type (Rhdf5Var), dimension(MaxFiles) :: InVars
+  type (Rhdf5Var), dimension(RHDF5_MAX_DIMS) :: Coords
+  type (Rhdf5Var) :: InTcoords
   type (Rhdf5Var) :: OutVar
 
-  integer :: i, iin, iout
+  integer :: i, iin, iout, itin, itout
+  integer :: Ndims
+  character (len=LittleString), dimension(RHDF5_MAX_DIMS) :: Cnames
+  character (len=LittleString), dimension(RHDF5_MAX_DIMS) :: GradsCnames
   
   real :: Xstart, Xinc, Ystart, Yinc
   logical :: BadData
@@ -101,6 +106,7 @@ program join_hdata
 
     if (i .eq. 1) then
       ! if first variable, then record the dimensions for comparing
+      ! and initialze the coordinate datasets
       Nx = InNx
       Ny = InNy
       Nz = InNz
@@ -136,14 +142,41 @@ program join_hdata
     endif
   enddo
 
+  Ndims = InVars(1)%ndims
 
   write (*,*) 'Variable dimensions after joining files:'
+  write (*,*) '  Ndims: ', Ndims
   write (*,*) '  Nx: ', Nx
   write (*,*) '  Ny: ', Ny
   write (*,*) '  Nz: ', Nz
   write (*,*) '  Nt: ', Nt
   write (*,*) ''
   flush(6)
+
+  ! Generate the names of the dimension coordinate variables
+  do i = 1, Ndims
+    Cnames(i) = trim(InVars(1)%dimnames(i)) // '_coords'
+    if (i .eq. 1) then
+      GradsCnames(i) = 'x'
+    else if (i .eq. 2) then
+      GradsCnames(i) = 'y'
+    else if (i .eq. 3) then
+      GradsCnames(i) = 'z'
+    else
+      GradsCnames(i) = 't'
+    endif
+  enddo 
+  ! the last dimension is always 't'
+  GradsCnames(Ndims) = 't'
+
+  ! Read in all but the last dimension from the first input file
+  !   Last dimension is 't' which will be handled below
+  do i = 1, Ndims-1
+    Coords(i)%vname = Cnames(i)
+    call rhdf5_read_init(InFileList(1), Coords(i))
+    allocate (Coords(i)%vdata(Coords(i)%dims(1)))
+    call rhdf5_read(InFileList(1), Coords(i))
+  enddo
 
   ! Initialize the output variable, go through the list of input variables and read
   ! in their data and append it to the output variable
@@ -159,11 +192,19 @@ program join_hdata
   OutVar%dims(Invars(1)%ndims) = Nt
   OutVar%units = Invars(1)%units
   OutVar%descrip = Invars(1)%descrip
-
   allocate(OutVar%vdata(Nx*Ny*Nz*Nt))
 
+  ! Set up the 't' coordinate variable based on contents of first input file
+  ! except make the size equal to Nt
+  Coords(Ndims)%vname = Cnames(Ndims)
+  call rhdf5_read_init(InFileList(1), Coords(Ndims))
+  Coords(Ndims)%dims(1) = Nt
+  allocate (Coords(Ndims)%vdata(Nt))
+
   iout = 0
+  itout = 0
   do i = 1, Nfiles
+    ! append the variable data
     allocate(InVars(i)%vdata(InSizes(i)))
     call rhdf5_read(InFileList(i), InVars(i))
     
@@ -173,12 +214,40 @@ program join_hdata
     end do
 
     deallocate(InVars(i)%vdata)
+
+    ! append the 't' coordinate variable data
+    InTcoords%vname = Cnames(Ndims)
+    call rhdf5_read_init(InFileList(i), InTcoords)
+    allocate(InTcoords%vdata(InTcoords%dims(1)))
+    call rhdf5_read(InFileList(i), InTcoords)
+
+    do itin = 1, InTcoords%dims(1)
+      itout = itout + 1
+      Coords(Ndims)%vdata(itout) = InTcoords%vdata(itin)
+    enddo
+
+    deallocate(InTcoords%vdata)
+    
   end do
 
   !Write out the joined data
-  call rhdf5_write(OutFile, OutVar)
+  call rhdf5_write(OutFile, OutVar, 0)
+
+  ! Write out the coordinate data and mark as such
+  ! variable
+  do i = 1, Ndims
+    call rhdf5_write(OutFile, Coords(i), 1)
+    call rhdf5_set_dimension(OutFile, Coords(i), GradsCnames(i))
+  enddo
+
+  ! attach the coordinates to the output variable
+  call rhdf5_attach_dimensions(OutFile, OutVar)
 
   deallocate(OutVar%vdata)
+!  deallocate(InTcoords)
+  do i = 1, Ndims
+    deallocate(Coords(i)%vdata)
+  enddo
 
   stop
 
