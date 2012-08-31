b2ff72e390cab582fa29f4f22d674853fe876500
steve <sherbener@atmos.colostate.edu>
2012-08-31 14:31:43 -0600

    Got rid of unused config for plotting in GRADS.

---
+++ src/DiagUtils.pm
@@ -0,0 +1,85 @@
+package DiagUtils;
+
+use strict;
+
+###############################################################################
+# ReadConfigFile()
+#
+# This routine will read in info from the diagnostics configuration file.
+# Hashes will be loaded up with the information and passed back to the caller.
+#
+
+sub ReadConfigFile
+  {
+  my ($ConfigFile) = @_;
+
+  my %Config;
+
+  my @f;
+
+  undef(%Config);
+
+  open(CONFIG, "$ConfigFile") or die "Cannot open '$ConfigFile' for reading: $!";
+  while (<CONFIG>)
+    {
+    @f = split(' ');
+  
+    if ($f[0] eq "Case:")
+      {
+      $Config{CASES}{$f[1]} = 1;
+      }
+    elsif ($f[0] eq "AzavgDir:")
+      {
+      $Config{AZAVG_DIR} = $f[1];
+      }
+    elsif ($f[0] eq "TsavgDir:")
+      {
+      $Config{TSAVG_DIR} = $f[1];
+      }
+    elsif ($f[0] eq "FilterDir:")
+      {
+      $Config{FILTER_DIR} = $f[1];
+      }
+    elsif ($f[0] eq "PlotDir:")
+      {
+      $Config{PLOT_DIR} = $f[1];
+      }
+    elsif ($f[0] eq "EofDir:")
+      {
+      $Config{EOF_DIR} = $f[1];
+      }
+    elsif ($f[0] eq "TimeDir:")
+      {
+      $Config{TIM_DIRS}{$f[1]} = $f[2];
+      }
+    elsif ($f[0] eq "Var:")
+      {
+      $Config{VARS}{$f[1]}{REVU_VAR} = $f[2];
+      $Config{VARS}{$f[1]}{T_SUFFIX} = $f[3];
+      }
+    elsif ($f[0] eq "Azavg:")
+      {
+      $Config{AZAVG_DIAGS}{$f[1]}{VAR}       = $f[2];
+      $Config{AZAVG_DIAGS}{$f[1]}{DIM}       = $f[3];
+      $Config{AZAVG_DIAGS}{$f[1]}{NBANDS}    = $f[4];
+      $Config{AZAVG_DIAGS}{$f[1]}{FILTER}    = $f[5];
+      }
+    elsif ($f[0] eq "Tsavg:")
+      {
+      $Config{TSAVG_DIAGS}{$f[1]}{FILTER} = $f[2];
+      }
+    elsif ($f[0] eq "Filter:")
+      {
+      $Config{FILTERS}{$f[1]} = [ @f[2..$#f] ];
+      }
+    elsif ($f[0] eq "Diag:")
+      {
+      $Config{DIAGS}{$f[1]}{$f[2]} = [ @f[3..$#f] ];
+      }
+    }
+  close(CONFIG);
+
+  return(\%Config);
+  }
+
+1;
