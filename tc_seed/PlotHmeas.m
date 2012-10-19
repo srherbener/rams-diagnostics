1a3ba8bdacc43e99565debfc2db1b9f088e19557
steve <sherbener@atmos.colostate.edu>
2012-10-19 12:45:32 -0600

    Added routines to plot histogram and CFAD data.

---
+++ tc_seed/PlotHmeas.m
@@ -0,0 +1,152 @@
+function [ ] = PlotHmeas(ConfigFile)
+% PlotHmeas function to plot total KE vs max azimuthally averaged tangential wind
+
+[ Config ] = ReadConfig(ConfigFile);
+
+InDir = Config.DiagDir;
+OutDir = Config.PlotDir;
+
+% Make sure output directory exists
+if (exist(OutDir, 'dir') ~= 7)
+    mkdir(OutDir);
+end
+
+Vtype = 'com'; % Center of mass measurement
+Rval = 50;  % radius = 50km
+Tval = 72;  % time = 72hrs
+
+Rmin = 0;
+Rmax = 250;
+Tmin = 24;
+Tmax = 144;
+
+Cmin = 0;
+Cmax = 100;
+Wmin = -1;
+Wmax = 1;
+LHmin = -1;
+LHmax = 1;
+
+% make the plots
+for icase = 1:length(Config.Cases)
+  Case = Config.Cases(icase).Cname;
+  for ihmeas = 1: length(Config.Hmeas)
+    Name = Config.Hmeas(ihmeas).Name;
+    Vname = Config.Hmeas(ihmeas).Rvar;
+    
+    fprintf('***********************************************************************\n');
+    fprintf('Plotting Histogram Measurements:\n');
+    fprintf('  Name: %s\n', Name);
+    fprintf('  Variable: %s\n', Vname);
+    fprintf('  Case: %s\n', Case);
+    fprintf('\n');
+
+    InFile = sprintf('%s/%s_%s.h5', InDir, Name, Case);
+    OutFileT = sprintf('%s/tseries_%s_%s.jpg', OutDir, Name, Case);
+    OutFileR = sprintf('%s/rseries_%s_%s.jpg', OutDir, Name, Case);
+    
+    % Read in the histogram data. HDATA will be organized as (r,z,t) where
+    %    r --> radius
+    %    z --> heights
+    %    t --> time
+    Hdset = sprintf('/%s_%s', Vname, Vtype);
+    fprintf('Reading file: %s, Dataset: %s\n', InFile, Hdset);
+    fprintf('\n');
+    R = hdf5read(InFile, '/x_coords');
+    Z = hdf5read(InFile, '/z_coords');
+    T = hdf5read(InFile, '/t_coords');
+    HDATA = hdf5read(InFile, Hdset);
+    
+    % Change time to hours, radius and height to km
+    R = R / 1000;
+    T = T / 3600;
+    Z = Z / 1000;
+    
+    % Set limits according to the quantity we are viewing
+    if (strcmp(Vname, 'ccn_conc'))
+        clims = [ Cmin, Cmax ];
+        Zmin = 0;
+        Zmax = 2.5;
+    end
+    if (strcmp(Vname, 'w'))
+        clims = [ Wmin, Wmax ];
+        Zmin = 0;
+        Zmax = 10;
+    end
+    if (strcmp(Vname, 'lh_vapt'))
+        clims = [ LHmin LHmax ];
+        Zmin = 0;
+        Zmax = 5;
+    end
+    if (strcmp(Vname, 'lh_frzt'))
+        clims = [ LHmin LHmax ];
+        Zmin = 0;
+        Zmax = 10;
+    end
+    
+    % Strip off the dimension according to Ptype
+    Rindex = find(R <= Rval, 1, 'last');
+    Tindex = find(T <= Tval, 1, 'last');
+    
+    R1 = find(R >= Rmin, 1, 'first');
+    R2 = find(R <= Rmax, 1, 'last');
+    Z1 = find(Z >= Zmin, 1, 'first');
+    Z2 = find(Z <= Zmax, 1, 'last');
+    T1 = find(T >= Tmin, 1, 'first');
+    T2 = find(T <= Tmax, 1, 'last');
+    
+    Tvals = T(T1:T2);
+    Rvals = R(R1:R2);
+    Zvals = Z(Z1:Z2);
+    
+    PdataT = squeeze(HDATA(Rindex,Z1:Z2,T1:T2));
+    PdataR = squeeze(HDATA(R1:R2,Z1:Z2,Tindex))';
+    
+    Ylabel = 'Height (km)';
+    
+    % Plot time series
+    Ptitle = sprintf('Time series, R = %dkm: %s', Rval, Vname);
+    Xlabel = 'Time (hr)';
+    Fig = figure;
+    contourf(Tvals,Zvals,PdataT);
+    set(gca, 'FontSize', 20);
+    if (~ strcmp(Vname, 'ccn_conc'))
+        colormap(redblue);
+    else
+        colormap('default');
+    end
+    shading flat;
+    colorbar;
+    caxis(clims);
+    title(Ptitle);
+    xlabel(Xlabel);
+    ylabel(Ylabel);
+    
+    saveas(Fig, OutFileT);
+    close(Fig);
+    
+    % Plot radial view
+    Ptitle = sprintf('Radial view, T = %dhr: %s', Tval, Vname);
+    Xlabel = 'Radius (km)';
+    Fig = figure;
+    contourf(Rvals,Zvals,PdataR);
+    set(gca, 'FontSize', 20);
+    if (~ strcmp(Vname, 'ccn_conc'))
+        colormap(redblue);
+    else
+        colormap('default');
+    end
+    shading flat;
+    colorbar;
+    caxis(clims);
+    title(Ptitle);
+    xlabel(Xlabel);
+    ylabel(Ylabel);
+    
+    saveas(Fig, OutFileR);
+    close(Fig);
+
+  end
+end
+
+end
