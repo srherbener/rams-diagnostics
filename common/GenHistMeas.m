78975dd8a58c550bbad9760713c38c9019d64db8
steve <sherbener@atmos.colostate.edu>
2012-11-19 16:02:13 -0700

    Fixed handling of missing data (which is the case where all counts in a given histogram are zero - ie no data was selected to be placed into the histogram).

--- common/GenHistMeas.m
+++ common/GenHistMeas.m
@@ -50,6 +50,7 @@ for icase = 1:length(Config.Cases)
     % mass). These can then be used in a time series or a series
     % across the storm radius.
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Do a weighted (by the histogram counts) average of the bin values.
     %   sum (bin(i) * counts(i)) / sum(counts(i)
     %
@@ -67,25 +68,37 @@ for icase = 1:length(Config.Cases)
     % Denominator
     % Sum along the bins dimension which holds the counts.
     % It's possible to get all counts equal to zero since we can filter
-    % out input data before making the counts. In the case where an entry
-    % in SUM_C comes out to be zero, it means all the counts were zero
-    % and for the purpose of this diagnostic we want the 0/0 operations
-    % to result in a zero instead of NaN. MATLAB rightly results in a NaN
-    % for 0/0, so we can force this to zero by changing all of the places
-    % where SUM_C == 0 to a non-zero number (now were doing 0/x where x
-    % is not zero).
+    % out input data before making the counts. In this case both SUM_BC
+    % and SUM_C will be zero and we will proceed to do a 0/0 operation.
+    % This results in a nan which is what we want so we can exclude this
+    % case from downstream analysis.
     SUM_C = squeeze(sum(HIST,2));
-    SUM_C(SUM_C == 0) = 1;
 
     HIST_WMEAN = SUM_BC ./ SUM_C;
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % At this point SUM_C (r,z,t) holds the sum of the counts for each
+    % histogram. For the max and center-of-mass calculations, create an
+    % array (r,z,t) that contains ones where there are non zero
+    % counts in the corresponding histogram, and nans where all bins
+    % in the corresponding historgram contain zero counts. This array can
+    % then be multiplied by the the result of the max and center-of-mass
+    % calculations in order to place nans where ever the histograms of all
+    % zero counts exist.
+    HAVE_COUNTS = SUM_C;
+    HAVE_COUNTS(HAVE_COUNTS ~= 0) = 1.0;
+    HAVE_COUNTS(HAVE_COUNTS == 0) = nan; 
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Find the indices of the max values along the bins dimension (number 2)
     % Then use that result to create an array organized as (r,z,t) that
     % contains the bin value corresponding to the index where the max count
     % existed.
     [ MAXC_V, MAXC_I ] = max(HIST,[],2);
     HIST_MAX = squeeze(B(MAXC_I));
+    HIST_MAX = HIST_MAX .* HAVE_COUNTS;
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % For every histogram (bins dimension: 2) find the index nearest where
     % the area under the curve is half the total area under the curve. Then
     % use these indices to place the corresponding bin values into the result.
@@ -103,12 +116,14 @@ for icase = 1:length(Config.Cases)
     CSHALF = abs(CSUM - (SUM/2));
     [ HALF_V, HALF_I ] = min(CSHALF, [], 2);
     HIST_COM = squeeze(B(HALF_I));
+    HIST_COM = HIST_COM .* HAVE_COUNTS;
 
-    % Also compute the time mean of the histogram measurements
+    % Also compute the time mean of the histogram measurements, ignoring
+    % the entries with nans.
     % All variables are (r,z,t), this will convert to (r,z).
-    HIST_WMEAN_TMEAN = mean(HIST_WMEAN,3);
-    HIST_MAX_TMEAN   = mean(HIST_MAX,3);
-    HIST_COM_TMEAN   = mean(HIST_COM,3);
+    HIST_WMEAN_TMEAN = nanmean(HIST_WMEAN,3);
+    HIST_MAX_TMEAN   = nanmean(HIST_MAX,3);
+    HIST_COM_TMEAN   = nanmean(HIST_COM,3);
 
     % If doing 'tempc'
     % Calculate a line representing the freezing level
