82286c64aabedbef9eddff34476a653d4379bba3
steve <sherbener@atmos.colostate.edu>
2012-11-20 15:36:12 -0700

    Added ability to specify a time range (Tmin, Tmax) to PlotHmeas2d that is used for doing time averaging. This way the rapid intensification phase and steady state phase of a TC can be separated. Move calculation of time averages from GenHistMeas into PlotHmeas2d since the time range for the averaging can vary. Moved calculation of freezing level from GenHistMeas into PlotHmeas2d since the time range can vary.

--- common/GenHistMeas.m
+++ common/GenHistMeas.m
@@ -118,40 +118,6 @@ for icase = 1:length(Config.Cases)
     HIST_COM = squeeze(B(HALF_I));
     HIST_COM = HIST_COM .* HAVE_COUNTS;
 
-    % Also compute the time mean of the histogram measurements, ignoring
-    % the entries with nans.
-    % All variables are (r,z,t), this will convert to (r,z).
-    HIST_WMEAN_TMEAN = nanmean(HIST_WMEAN,3);
-    HIST_MAX_TMEAN   = nanmean(HIST_MAX,3);
-    HIST_COM_TMEAN   = nanmean(HIST_COM,3);
-
-    % If doing 'tempc'
-    % Calculate a line representing the freezing level
-    % Don't use >= 0 in the find command because the wtmean method
-    % can result in multiple zeros in a profile
-    if (strcmp(Vname, 'tempc'))
-      FRZLEV_WMEAN = zeros(1,Nr);
-      FRZLEV_MAX = zeros(1,Nr);
-      FRZLEV_COM = zeros(1,Nr);
-      for ir = 1:Nr
-        i = find(HIST_WMEAN_TMEAN(ir,:) > 0, 1, 'last');
-        if (length(i) == 0)
-          i = 1;
-        end
-        FRZLEV_WMEAN(ir) = Z(i);
-        i = find(HIST_MAX_TMEAN(ir,:) > 0, 1, 'last');
-        if (length(i) == 0)
-          i = 1;
-        end
-        FRZLEV_MAX(ir)   = Z(i);
-        i = find(HIST_COM_TMEAN(ir,:) > 0, 1, 'last');
-        if (length(i) == 0)
-          i = 1;
-        end
-        FRZLEV_COM(ir)   = Z(i);
-      end
-    end
-
     fprintf('Writing file: %s\n', OutFile);
     fprintf('\n');
 
@@ -161,22 +127,6 @@ for icase = 1:length(Config.Cases)
     hdf5write(OutFile, Hdset, HIST_MAX, 'WriteMode', 'append');
     Hdset = sprintf('/%s_com', Vname);
     hdf5write(OutFile, Hdset, HIST_COM, 'WriteMode', 'append');
-
-    Hdset = sprintf('/%s_wtmean_tmean', Vname);
-    hdf5write(OutFile, Hdset, HIST_WMEAN_TMEAN, 'WriteMode', 'append');
-    Hdset = sprintf('/%s_max_tmean', Vname);
-    hdf5write(OutFile, Hdset, HIST_MAX_TMEAN, 'WriteMode', 'append');
-    Hdset = sprintf('/%s_com_tmean', Vname);
-    hdf5write(OutFile, Hdset, HIST_COM_TMEAN, 'WriteMode', 'append');
-
-    if (strcmp(Vname, 'tempc'))
-      Hdset = sprintf('/frzlev_wtmean');
-      hdf5write(OutFile, Hdset, FRZLEV_WMEAN, 'WriteMode', 'append');
-      Hdset = sprintf('/frzlev_max');
-      hdf5write(OutFile, Hdset, FRZLEV_MAX, 'WriteMode', 'append');
-      Hdset = sprintf('/frzlev_com');
-      hdf5write(OutFile, Hdset, FRZLEV_COM, 'WriteMode', 'append');
-    end
 
     hdf5write(OutFile, '/x_coords', R, 'WriteMode', 'append');
     hdf5write(OutFile, '/y_coords', B, 'WriteMode', 'append');
--- tc_seed/PlotHmeas2d.m
+++ tc_seed/PlotHmeas2d.m
@@ -16,7 +16,8 @@ end
 Fsize = 20;
 
 % Temperature file 
-TempFile = 'tempc_all';
+TempFile = 'tempc';
+TempVar = 'tempc';
 
 % Get some colormaps
 Fig = figure;
@@ -26,6 +27,7 @@ close(Fig);
 
 % make the plots
 % steal the config for the 3d plots, simply ignore the isosurface spec
+% use the Tmin, Tmax to do time averaging between a specified interval
 for icase = 1:length(Config.Cases)
   Case = Config.Cases(icase).Cname;
   CaseTitle = regexprep(Case, '_', '-');
@@ -62,6 +64,7 @@ for icase = 1:length(Config.Cases)
     fprintf('  Variable: %s\n', Vname);
     fprintf('  Case: %s\n', Case);
     fprintf('  Plot type: %s\n', Ptype);
+    fprintf('  Time averaging bounds: %.2f to %.2f\n', Tmin, Tmax);
     fprintf('\n');
 
     InFile = sprintf('%s/hmeas_%s_%s.h5', InDir, Fprefix, Case);
@@ -70,32 +73,32 @@ for icase = 1:length(Config.Cases)
     % Read in the histogram data. HDATA will be organized as (r,z) where
     %    r --> radius
     %    z --> heights
-    Hdset = sprintf('/%s_%s_tmean', Vname, Vtype);
+    Hdset = sprintf('/%s_%s', Vname, Vtype);
     fprintf('Reading file: %s, Dataset: %s\n', InFile, Hdset);
     fprintf('\n');
     R = hdf5read(InFile, '/x_coords') / 1000; % km
     Z = hdf5read(InFile, '/z_coords') / 1000; % km
+    T = hdf5read(InFile, '/t_coords') / 3600; % hr
     HDATA = hdf5read(InFile, Hdset);
 
-    % if adding a freezing level line, read in the freezing level data
-    % from the temperature file
-    %
-    % FL will have the same dimensions as R
+    % if adding a freezing level line, generate it from the data in
+    % the temperature file
     if (Flevel == 1)
       InFile = sprintf('%s/hmeas_%s_%s.h5', InDir, TempFile, Case);
-      Hdset = sprintf('/frzlev_%s', Vtype);
+      Hdset = sprintf('/%s_%s', TempVar, Vtype);
       fprintf('Reading file: %s, Dataset: %s\n', InFile, Hdset);
       fprintf('\n');
-      FL = hdf5read(InFile, Hdset) / 1000; % km
+      TEMP = hdf5read(InFile, Hdset) / 1000; % km
     end
 
     % If doing a 'diff' plot, read in the control data
     if (strcmp(Ptype, 'diff'))
       InFile = sprintf('%s/hmeas_%s_%s.h5', InDir, Fprefix, ControlCase);
-      Hdset = sprintf('/%s_%s_tmean', Vname, Vtype);
+      Hdset = sprintf('/%s_%s', Vname, Vtype);
       fprintf('Reading file: %s, Dataset: %s\n', InFile, Hdset);
       fprintf('\n');
       CNTL_HDATA = hdf5read(InFile, Hdset);
+      T_CNTL = hdf5read(InFile, '/t_coords') / 3600; % hr
     end
     
     % find the indices that go with the specified limits 
@@ -103,14 +106,36 @@ for icase = 1:length(Config.Cases)
     R2 = find(R <= Rmax, 1, 'last');
     Z1 = find(Z >= Zmin, 1, 'first');
     Z2 = find(Z <= Zmax, 1, 'last');
+    T1 = find(T >= Tmin, 1, 'first');
+    T2 = find(T <= Tmax, 1, 'last');
+    if (strcmp(Ptype, 'diff'))
+      T1_CNTL = find(T_CNTL >= Tmin, 1, 'first');
+      T2_CNTL = find(T_CNTL <= Tmax, 1, 'last');
+    end
 
     Rvals = R(R1:R2);
     Zvals = Z(Z1:Z2);
+
+    % Gererate the freezing level from the time average of the temperature.
     if (Flevel == 1)
-      FLvals = FL(R1:R2);
+      TempMean = squeeze(nanmean(TEMP(R1:R2,Z1:Z2,T1:T2),3));
+
+      % TempMean is now (r,z)
+      % Calculate a line representing the freezing level
+      % Don't use >= 0 in the find command because the wtmean method
+      % can result in multiple zeros in a profile
+      [ Nr, Nz ] = size(TempMean);
+      Flvals = zeros(1,Nr);
+      for ir = 1:Nr
+        i = find(TempMean(ir,:) > 0, 1, 'last');
+        if (length(i) == 0)
+          i = 1;
+        end
+        FLvals(ir) = Zvals(i);
+      end
     end
     
-    Pdata = squeeze(HDATA(R1:R2,Z1:Z2));
+    Pdata = squeeze(nanmean(HDATA(R1:R2,Z1:Z2,T1:T2),3));
     
     % switch (r,z) to (z,r)
     Pdata = permute(Pdata, [ 2 1 ]);
@@ -119,7 +144,7 @@ for icase = 1:length(Config.Cases)
     if (strcmp(Ptype, 'diff'))
       fprintf('Subtracting off control case: %s\n', ControlCase);
       fprintf('\n');
-      CntlPdata = squeeze(CNTL_HDATA(R1:R2,Z1:Z2));
+      CntlPdata = squeeze(nanmean(CNTL_HDATA(R1:R2,Z1:Z2,T1_CNTL:T2_CNTL),3));
       CntlPdata = permute(CntlPdata, [ 2 1 ]);
       Pdata = Pdata - CntlPdata;
     end
