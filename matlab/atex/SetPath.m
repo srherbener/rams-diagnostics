f62a32819274dde8aaa0e3b7c6df239e12a358f0
steve <sherbener@atmos.colostate.edu>
2013-08-01 17:38:24 -0600

    Added routine to plot the initial sounding for the simulation runs.

--- matlab/atex/SetPath.m
+++ matlab/atex/SetPath.m
@@ -1,6 +1,6 @@
 % Script to set up path to use the atex routines
 
 restoredefaultpath;
-addpath('~/etc/matlab/common', '~/etc/matlab/m_map', '~/etc/matlab/obj_anal', '~/etc/matlab/atex', '-end');
+addpath('~/etc/matlab/common', '~/etc/matlab/m_map', '~/etc/matlab/obj_anal', '~/etc/matlab/atex', '~/etc/matlab/metpack', '-end');
 
 run ~/etc/matlab/nctoolbox/setup_nctoolbox.m;
