4e3ac2967dca8a98f5e35a32bd1baa2b7b7ace21
steve <sherbener@atmos.colostate.edu>
2013-10-14 17:04:12 -0600

    Replicated plots using tangential velocity, replacing the tangential velocity with the estimated 10m wind speed from REVU.

--- matlab/tc_seed/PlotControlVtSpl.m
+++ matlab/tc_seed/PlotControlVtSpl.m
@@ -1,6 +1,8 @@
-function [ ] = PlotControlVtSpl(ConfigFile)
+function [ ] = PlotControlVtSpl(ConfigFile, InType)
 % PlotControlVtSpl function to plot max sfc Vt and SPL of control simulation
 %
+
+UseUv = strcmp(InType, 'uv');
 
 [ Config ] = ReadConfig(ConfigFile);
 
@@ -23,12 +25,27 @@ end
 % VT will be (r,z,t), SPL will be (p,t)
 
 % read in the Vt and time coordinate data
-Hfile = sprintf('%s/speed_t_%s.h5', AzavgDir, SpinUpCase);
-VT_SPINUP = squeeze(hdf5read(Hfile, '/speed_t'));
+if (UseUv)
+  Hfile = sprintf('%s/speed_t_%s.h5', AzavgDir, SpinUpCase);
+  VT_SPINUP = squeeze(hdf5read(Hfile, '/speed_t'));
+else
+  Hfile = sprintf('%s/speed10m_%s.h5', AzavgDir, SpinUpCase);
+  VT_SPINUP = squeeze(hdf5read(Hfile, '/speed10m'));
+end
 T = squeeze(hdf5read(Hfile, '/t_coords'));
-Hfile = sprintf('%s/speed_t_%s.h5', AzavgDir, ControlCase);
-VT_CNTL = squeeze(hdf5read(Hfile, '/speed_t'));
-VT = cat(3, VT_SPINUP(:,:,1:24), VT_CNTL);
+if (UseUv)
+  Hfile = sprintf('%s/speed_t_%s.h5', AzavgDir, ControlCase);
+  VT_CNTL = squeeze(hdf5read(Hfile, '/speed_t'));
+else
+  Hfile = sprintf('%s/speed10m_%s.h5', AzavgDir, ControlCase);
+  VT_CNTL = squeeze(hdf5read(Hfile, '/speed10m'));
+end
+
+if (UseUv)
+  VT = cat(3, VT_SPINUP(:,:,1:24), VT_CNTL);
+else
+  VT = cat(2, VT_SPINUP(:,1:24), VT_CNTL);
+end
 
 % read in the SPL
 Hfile = sprintf('%s/sea_press_%s.h5', AzavgDir, SpinUpCase);
@@ -39,7 +56,11 @@ SPL = cat(2, SPL_SPINUP(:,1:24), SPL_CNTL);
 
 % Create a time series of the max sfc Vt and one of the min SPL
 % z == 1 is below sfc, so use z == 2 for sfc Vt
-VT = squeeze(VT(:,2,:)); % take the z == 2 level
+if (UseUv)
+  VT = squeeze(VT(:,2,:)); % take the z == 2 level
+else
+  VT = squeeze(VT(:,:));
+end
 
 MIN_SPL = min(SPL,[],1);
 MAX_VT  = max(VT,[],1);
@@ -88,7 +109,11 @@ MIN_SPL = SmoothFillTseries(MIN_SPL, Ntsteps, Flen);
 % Plot
 Lwidth = 3;
 Fsize = 45;
-Pfile = sprintf('%s/ControlVtSpl.jpg', PlotDir);
+if (UseUv)
+  Pfile = sprintf('%s/ControlVtSpl.jpg', PlotDir);
+else
+  Pfile = sprintf('%s/ControlVtSpl_s10.jpg', PlotDir);
+end
 Xlabel = sprintf('Simulation Time (hr)');
 Y1label = sprintf('Max Vt (m/s)');
 Y2label = sprintf('Min SLP (mb)');
@@ -99,8 +124,13 @@ Xlims = [ 0 150 ];
 % Set up plot controls according to SST
 switch SstVal
   case 26
-    Y1lims = [ 0 50 ];
-    Y2lims = [ 970 1020 ];
+    if (UseUv)
+      Y1lims = [ 0 50 ];
+      Y2lims = [ 970 1020 ];
+    else
+      Y1lims = [ 0 60 ];
+      Y2lims = [ 960 1020 ];
+    end
     Tbefore = 24;
     Tduring = 54;
     Tafter = 84;
@@ -158,7 +188,11 @@ set(gca, 'TickLength', [ 0.025 0.025 ]);
 set(gca, 'Position', AxisPos);
 set(gca, 'YColor', 'k');
 set(gca, 'XTick', [ 50 100 ]);
-set(gca, 'YTick', [ 0 20 40 ]);
+if (UseUv)
+  set(gca, 'YTick', [ 0 20 40 ]);
+else
+  set(gca, 'YTick', [ 0 20 40 60 ]);
+end
 xlim(Xlims);
 ylabel(Y1label);
 ylim(Y1lims);
@@ -170,7 +204,11 @@ set(gca, 'TickLength', [ 0.025 0.025 ]);
 set(gca, 'Position', AxisPos);
 set(gca, 'YColor', 'k');
 set(gca, 'XTick', [ 50 100 ]);
-set(gca, 'YTick', [ 970 990 1010 ]);
+if (UseUv)
+  set(gca, 'YTick', [ 970 990 1010 ]);
+else
+  set(gca, 'YTick', [ 960 980 1000 1020 ]);
+end
 xlim(Xlims);
 ylabel(Y2label);
 ylim(Y2lims);
