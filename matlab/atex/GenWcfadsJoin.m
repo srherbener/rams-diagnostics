2831f8aa9d1e664b6702f94d66df4d3aa046539a
steve <sherbener@atmos.colostate.edu>
2014-01-21 12:46:13 -0700

    Added script to join the updraft and downdraft histograms into one histogram that is subsequently used to create a CFAD.

--- matlab/atex/GenWcfadsJoin.m
+++ matlab/atex/GenWcfadsJoin.m
@@ -1,5 +1,5 @@
-function [ ] = GenWcfads(ConfigFile)
-% GenWcfads generate cfads of w data averaged over a time interval
+function [ ] = GenWcfadsJoin(ConfigFile)
+% GenWcfadsJoin generate cfads of w data over a time interval, join two input files together to create the cfad
 
     % Read the config file to get the structure of how the data is laid out in
     % the file system.
@@ -10,13 +10,17 @@ function [ ] = GenWcfads(ConfigFile)
 
     Wname = 'hist_w';
 
-    Wfcases = {
-     'hist_w'
-%     'hist_w_nz'
+    UPfcases = {
+     'hist_up_0p01'
+     'hist_up_0p10'
+     };
+    DNfcases = {
+     'hist_dn_0p01'
+     'hist_dn_0p10'
      };
     Ofcases = {
-     'cfad_w'
-%     'cfad_w_nz'
+     'cfad_w_0p01'
+     'cfad_w_0p10'
      };
 
     TstartTimes = [
@@ -30,29 +34,48 @@ function [ ] = GenWcfads(ConfigFile)
      36
      ];
 
+    GroupSize = 20;
+
     for icase = 1:length(Config.Cases)
         Case = Config.Cases(icase).Cname;
 
-        for ifc = 1:length(Wfcases)
-            Wfcase = Wfcases{ifc};
+        for ifc = 1:length(UPfcases)
+            UPfcase = UPfcases{ifc};
+            DNfcase = DNfcases{ifc};
             Ofcase = Ofcases{ifc};
 
-            Wfile   = sprintf('%s/%s_%s.h5', Tdir, Wfcase, Case);
+            UPfile   = sprintf('%s/%s_%s.h5', Tdir, UPfcase, Case);
+            DNfile   = sprintf('%s/%s_%s.h5', Tdir, DNfcase, Case);
     
             fprintf('***************************************************************\n');
             fprintf('Generating w cfad:\n');
             fprintf('  Case: %s\n', Case);
-            fprintf('  Input w histogram file: %s\n', Wfile);
+            fprintf('  Input updraft histogram file: %s\n', UPfile);
+            fprintf('    Var name: %s\n', Wname);
+            fprintf('  Input downdraft histogram file: %s\n', DNfile);
             fprintf('    Var name: %s\n', Wname);
             fprintf('\n');
 
             % Read in data
             %  NOTE: T is in seconds and Tstart,Tend are in hours
-            W_HIST = squeeze(hdf5read(Wfile, Wname));
-            X = squeeze(hdf5read(Wfile, '/x_coords'));
-            Y = 1; % Y is a dummy variable
-            Z = squeeze(hdf5read(Wfile, '/z_coords'));
-            T = squeeze(hdf5read(Wfile, '/t_coords')) / 3600;  % hours
+            UP_HIST = squeeze(hdf5read(UPfile, Wname));
+            UP_X = squeeze(hdf5read(UPfile, '/x_coords'));
+            DN_HIST = squeeze(hdf5read(DNfile, Wname));
+            DN_X = squeeze(hdf5read(DNfile, '/x_coords'));
+            Z = squeeze(hdf5read(DNfile, '/z_coords'));
+            T = squeeze(hdf5read(DNfile, '/t_coords')) / 3600;  % hours
+
+            % Combine histogram bins in the updraft and downdraft data, then join the two to create on histogram.
+            % The last bin of the downdraft histogram is for w = 0. This bin will always contain a count of zero
+            % due to the filtering out of zero valued vertical velocity. Therefore, throw away this bin.
+            fprintf('  Combining histogram bins, group size: %d\n', GroupSize);
+            fprintf('\n');
+
+            UP = CombineBins(UP_HIST, 1, GroupSize);
+            DN = CombineBins(DN_HIST(1:end-1,:,:), 1, GroupSize); % strip off the last bin (w == zero)
+            W_HIST = cat(1, DN, UP);
+            DN_X_TEMP = DN_X(1:end-1);  % strip off the last bin (w == zero)
+            X = cat(1, DN_X_TEMP(1:GroupSize:end), UP_X(1:GroupSize:end));
 
             for it = 1:length(TstartTimes)
               Tstart = TstartTimes(it);
@@ -77,12 +100,13 @@ function [ ] = GenWcfads(ConfigFile)
 
               % output --> Use REVU format, 4D var, *_coords
               Tdummy = 1;  % due to summing over time, t becomes a dummy variable
+              Ydummy = 1; % Y is a dummy variable
 
               OUT_W_CFAD     = reshape(W_CFAD,     [ Nx 1 Nz 1 ]);
               fprintf('  Writing: %s\n', OutFile);
               hdf5write(OutFile, '/W_CFAD',     OUT_W_CFAD);
               hdf5write(OutFile, '/x_coords',   X,           'WriteMode', 'append');
-              hdf5write(OutFile, '/y_coords',   Y,           'WriteMode', 'append');
+              hdf5write(OutFile, '/y_coords',   Ydummy,      'WriteMode', 'append');
               hdf5write(OutFile, '/z_coords',   Z,           'WriteMode', 'append');
               hdf5write(OutFile, '/t_coords',   Tdummy,      'WriteMode', 'append');
               fprintf('\n');
--- matlab/common/CombineBins.m
+++ matlab/common/CombineBins.m
@@ -39,16 +39,56 @@ function [ Hcombined ] = CombineBins( Harray, Hdim, GroupSize )
   Indices = [ i; j; k; l ];
 
   % Change the vector (row) in Indices corresponding to the dimension given in Hdim 
-  % so that it will collapse that dimension when accumarra()y is called.
+  % so that it will collapse that dimension when accumarray() is called. At this point,
+  % Indices contains the sequence of index values corresponding to walking through Harray
+  % in column-major order (ie, matches the order given by Harray(:)). What we want to do
+  % is change the indices in the row of Indices so that when Indices is given to accumarray()
+  % adjacent elements along Hdim will be added together. Say Harray is 3x3 and Hdim is 2 and
+  % group size is 2. Indices starts out containing:
+  %
+  %     1 2 3 1 2 3 1 2 3
+  %     1 1 1 2 2 2 3 3 3
+  %
+  % We want Indices to be changed to:
+  %
+  %     1 2 3 1 2 3 1 2 3
+  %     1 1 1 1 1 1 2 2 2
+  %
+  % which tells accumarray() to add together the first two elements along the second dimension,
+  % (second row of Indices) to form the 1st element of the output array (in its second dimension)
+  % and to add the 3rd elements of Harray, 2nd dimension to form the 2nd element in the output. If
+  % Harray started out as:
+  %
+  %      1 4 7
+  %      2 5 8
+  %      3 6 9
+  %
+  % then using the above example, the output of accumarray() will be: 
+  %
+  %      5 7
+  %      7 8
+  %      9 9
+  %
+  % where the first two columns (1st two elements of the 2nd dimension) of Harray got added together.
   %
   % Temporarily reduce the dimension size to an even multiple of GroupSize. Then figure out
   % the pattern for selecting adjacent blocks (of size GroupSize) out of the dimension. If the original
   % dimension size didn't divide evenly by GroupSize, then tack on the extras needed to combine the remaining
   % entries into an extra output element.
 
-  % Form the access pattern though the size of the selected dimension.
+  % Form the access pattern through the size of the selected dimension.
   % Note that if Hsize(Hdim) / GroupSize does not divide evenly, that we will need to tag on
   % an extra piece at the end of the vector.
+  % Example:
+  %   Let's say Hdim = 2 and the size of dimension 2 is 10.
+  %
+  %   If group size is 3, then we want NewVector to be: 1 1 1 2 2 2 3 3 3 4
+  %   which says to accumarray() to add the first 3 elements together (1 1 1) to form the first
+  %   element of the output array, the next 3 elements together (2 2 2) to form the second element
+  %   of the output array, then next three together (3 3 3) to form the third element of the output,
+  %   and add the last element (4) to form the fourth element of the output.
+  %
+  %   If group size is 2, then we want NewVector to be: 1 1 2 2 3 3 4 4 5 5
   RepLength = floor(Hsize(Hdim) / GroupSize);
   VectRem = mod(Hsize(Hdim), GroupSize);
   NewVector = 1:RepLength;
@@ -56,6 +96,9 @@ function [ Hcombined ] = CombineBins( Harray, Hdim, GroupSize )
   NewVector = [ NewVector(:)' ones([ 1 VectRem ]).*(RepLength+1) ];
 
   % Replicate the pattern within the context of where its dimension fits into the total array.
+  % Note that it is important for NewVector to be a row vector during this section. This makes
+  % the pattern come out correct when changing the output of repmat() to a 1D vector in the
+  % NewVector(:) statement.
   RowRep = prod([ 1 Hsize(1:Hdim-1) ]);
   ColRep = prod([ 1 Hsize(Hdim+1:end) ]);
 
