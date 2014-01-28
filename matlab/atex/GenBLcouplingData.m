ba06763cd5e723f49e2278c00b57ddd7122e3646
steve <sherbener@atmos.colostate.edu>
2014-01-28 15:10:57 -0700

    Added filtered cloud mixing ratio averages   grid cells with rc > 0.01   grid cells with rc > 0.10

--- matlab/atex/GenBLcouplingData.m
+++ matlab/atex/GenBLcouplingData.m
@@ -17,12 +17,16 @@ function [ ] = GenBLcouplingData(ConfigFile)
     TDfprefix = 'sfc_dewptc';
     CldName = 'hda_cloud';
     CldFprefix = 'hda_cloud';
+    Cld_c0p01_fprefix = 'hda_cloud_c0p01';
+    Cld_c0p10_fprefix = 'hda_cloud_c0p10';
 
     for icase = 1:length(Config.Cases)
 	Case = Config.Cases(icase).Cname;
 	Tfile = sprintf('%s/%s-%s-AS-1999-02-10-040000-g1.h5', Hdir, Tfprefix, Case);
 	TDfile = sprintf('%s/%s-%s-AS-1999-02-10-040000-g1.h5', Hdir, TDfprefix, Case);
 	CldFile = sprintf('%s/%s_%s.h5', Tdir, CldFprefix, Case);
+	Cld_c0p01_file = sprintf('%s/%s_%s.h5', Tdir, Cld_c0p01_fprefix, Case);
+	Cld_c0p10_file = sprintf('%s/%s_%s.h5', Tdir, Cld_c0p10_fprefix, Case);
 
         fprintf('***************************************************************\n');
         fprintf('Generating BL coupling data:\n');
@@ -32,6 +36,8 @@ function [ ] = GenBLcouplingData(ConfigFile)
         fprintf('  Input dewpoint file: %s\n', TDfile);
         fprintf('    Var name: %s\n', TDname);
         fprintf('  Input cloud file: %s\n', CldFile);
+        fprintf('  Input cloud file: %s\n', Cld_c0p01_file);
+        fprintf('  Input cloud file: %s\n', Cld_c0p10_file);
         fprintf('    Var name: %s\n', CldName);
         fprintf('\n');
  
@@ -41,7 +47,10 @@ function [ ] = GenBLcouplingData(ConfigFile)
         %     y(2) --> total number of points used to create y(1)
         %   z is height
         %   t is time
-        CLD = squeeze(hdf5read(CldFile, CldName));
+        CLD       = squeeze(hdf5read(CldFile, CldName));
+        CLD_C0P01 = squeeze(hdf5read(Cld_c0p01_file, CldName));
+        CLD_C0P10 = squeeze(hdf5read(Cld_c0p10_file, CldName));
+
         TEMP = squeeze(hdf5read(Tfile, Tname));
         TEMPD = squeeze(hdf5read(TDfile, TDname));
 
@@ -50,23 +59,44 @@ function [ ] = GenBLcouplingData(ConfigFile)
         T = squeeze(hdf5read(Tfile, 't_coords'));
         Nt = length(T);
 
-        % Range to look for cloud base
-        Z1 = find(Z >= 250, 1, 'first');
-        Z2 = find(Z <= 2500, 1, 'last');
- 
+        % Form an array (z,t) that shows where Z > some minimum height for the cloud base
+        Z_FILTER = repmat(Z, [ 1 Nt ]) > 200;
+
         % cloud base
         %   calculate average profile
         %   find the index of the first point (from sfc) that has cloud profile > 0.01 g/kg
         % CLD_PROF will be (z,t)
-        CLD_PROF = squeeze(CLD(1,Z1:Z2,:) ./ CLD(2,Z1:Z2,:));
+        CLD_PROF = squeeze(CLD(1,:,:) ./ CLD(2,:,:));
+        CLD_C0P01_PROF = squeeze(CLD_C0P01(1,:,:) ./ CLD_C0P01(2,:,:));
+        CLD_C0P10_PROF = squeeze(CLD_C0P10(1,:,:) ./ CLD_C0P10(2,:,:));
+
+        % Form logical arrays that have 1's where the clouds exist, except
+        % exclude the near surface levels (Z_FILTER).
+        % Cloud base is then where the lowest level 1 exists in each column
+        % of SELECT.
+        SELECT = CLD_PROF > 0.01 & Z_FILTER;
+        SELECT_C0P01 = CLD_C0P01_PROF > 0.01 & Z_FILTER;
+        SELECT_C0P10 = CLD_C0P10_PROF > 0.01 & Z_FILTER;
+
         CLD_BASE = zeros([ 1 Nt ]);
+        CLD_C0P01_BASE = zeros([ 1 Nt ]);
+        CLD_C0P10_BASE = zeros([ 1 Nt ]);
         for it = 1:Nt
-          ZB = find(squeeze(CLD_PROF(:,it) > 0.01), 1, 'first');
+          S = squeeze(SELECT(:,it));
+          S_C0P01 = squeeze(SELECT_C0P01(:,it));
+          S_C0P10 = squeeze(SELECT_C0P10(:,it));
+
+          ZB       = find(S, 1, 'first');
+          ZB_C0P01 = find(S_C0P01, 1, 'first');
+          ZB_C0P10 = find(S_C0P10, 1, 'first');
           if (isempty(ZB))
             CLD_BASE(it) = nan;
+            CLD_C0P01_BASE(it) = nan;
+            CLD_C0P10_BASE(it) = nan;
           else
-            ZB = ZB + Z1 - 1;
             CLD_BASE(it) = Z(ZB);
+            CLD_C0P01_BASE(it) = Z(ZB_C0P01);
+            CLD_C0P10_BASE(it) = Z(ZB_C0P10);
           end
         end
 
@@ -87,6 +117,12 @@ function [ ] = GenBLcouplingData(ConfigFile)
 
         Ovar = reshape(CLD_BASE, [ 1 1 1 Nt ]);
         hdf5write(OutFile, '/avg_cloud_base', Ovar, 'WriteMode', 'append');
+
+        Ovar = reshape(CLD_C0P01_BASE, [ 1 1 1 Nt ]);
+        hdf5write(OutFile, '/avg_cloud_base_c0p01', Ovar, 'WriteMode', 'append');
+
+        Ovar = reshape(CLD_C0P10_BASE, [ 1 1 1 Nt ]);
+        hdf5write(OutFile, '/avg_cloud_base_c0p10', Ovar, 'WriteMode', 'append');
 
         hdf5write(OutFile, 'x_coords', X, 'WriteMode', 'append');
         hdf5write(OutFile, 'y_coords', Y, 'WriteMode', 'append');
