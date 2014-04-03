1a1aa15f635a8fef7819948d3e9b146451d8aa18
steve <sherbener@atmos.colostate.edu>
2014-04-03 01:03:26 -0600

    Added script to form time averages from the hda data.

---
+++ matlab/atex/GenAvgFilesCtype.m
@@ -0,0 +1,110 @@
+function [ ] = GenAvgFilesCtype(ConfigFile)
+% GenAvgFilesCtype generate averages from tsavg hda files
+
+  % Read the config file to get the structure of how the data is laid out in
+  % the file system.
+  [ Config ] = ReadConfig(ConfigFile);
+
+  Tdir = Config.TsavgDir;
+  Ddir = Config.DiagDir;
+
+  FileHeader = 'ATEX PDF data';
+
+  VarSets = {
+    { 'hda_pcprr'           { 'pcprr'  'pcprr_strat'  'pcprr_scmix'  'pcprr_cumul'  } 1 2 'avg_ctype_pcprr'  }
+    { 'hda_cloud_opt_thick' { 'cot'    'cot_strat'    'cot_scmix'    'cot_cumul'    } 1 2 'avg_ctype_cot'    }
+    { 'hda_cloud_depth'     { 'cdepth' 'cdepth_strat' 'cdepth_scmix' 'cdepth_cumul' } 1 2 'avg_ctype_cdepth' }
+    { 'hda_vint_cond'       { 'lwp'    'lwp_strat'    'lwp_scmix'    'lwp_cumul'    } 1 2 'avg_ctype_lwp'    }
+    };
+  Nset = length(VarSets);
+
+  TimeSelects = {
+    { 12    36    'TALL'   }      
+    { 12    13    'TSTART' }
+    { 23.5  24.5  'TMID'   }
+    { 35    36    'TEND'   }
+    };
+  Ntsel = length(TimeSelects);
+
+  for icase = 1:length(Config.Cases)
+    Case = Config.Cases(icase).Cname;
+
+    fprintf('***************************************************************\n');
+    fprintf('Generating avg data:\n');
+    fprintf('  Case: %s\n', Case);
+
+    for iset = 1:Nset
+      InVarName   = VarSets{iset}{1};
+      OutVarList  = VarSets{iset}{2};
+      SumInd      = VarSets{iset}{3};
+      NptsInd     = VarSets{iset}{4};
+      OutFprefix  = VarSets{iset}{5};
+
+      fprintf('    Input files:\n');
+
+      % Write a header into the file so that a write statement without append mode
+      % will be run first which consequently will erase an existing file and replace
+      % it with the contents about to be generated here.
+      OutFile = sprintf('%s/%s_%s.h5', Ddir, OutFprefix, Case);
+      hdf5write(OutFile, 'Header', FileHeader); 
+
+      Nvar = length(OutVarList);
+      for ivar = 1:Nvar
+        OutVarName = OutVarList{ivar};
+
+        InFile = sprintf('%s/hda_%s_%s.h5', Tdir, OutVarName , Case);
+        fprintf('      %s --> %s\n', InFile, InVarName);
+
+        % grab the t coordinates, use these for combinations
+        T = squeeze(hdf5read(InFile, 't_coords'))/3600; % hours
+
+        OutAvgName  = sprintf('%s_avg', OutVarName);
+        OutNptsName = sprintf('%s_npts', OutVarName);
+
+        for its = 1:Ntsel
+          Tstart = TimeSelects{its}{1};
+          Tend   = TimeSelects{its}{2};
+          Tname  = TimeSelects{its}{3};
+
+          T1 = find(T >= Tstart, 1, 'first');
+          T2 = find(T <= Tend,   1, 'last');
+
+          % sum up bins across selected times, and convert counts to PDF
+          % VAR will be organized as (2,t) where the first dimension denotes
+          % sums and npts, and t is time
+          VAR  = squeeze(hdf5read(InFile, InVarName));
+          SUM  = squeeze(sum(VAR(SumInd,T1:T2)));
+          NPTS = squeeze(sum(VAR(NptsInd,T1:T2)));
+
+          AVG = SUM ./ NPTS;
+
+          % With the data selection it is possible to get no data points selected (all counts
+          % equal to zero in hda file). When this happens, get nans in Avg since the sum
+          % is zero. Ie, the AVG entry is 0/0 --> nan. Leave as nan to signal downstream
+          % processes that the AVG is empty.
+
+          OutName = sprintf('%s_%s', OutAvgName, Tname);
+          hdf5write(OutFile, OutName, AVG, 'WriteMode', 'append');
+
+          OutName = sprintf('%s_%s', OutNptsName, Tname);
+          hdf5write(OutFile, OutName, NPTS, 'WriteMode', 'append');
+        end
+      end
+      fprintf('\n');
+
+      % output coords so that ReadSelectXyzt can handle the output files
+      Xdummy = 1;
+      Ydummy = 1;
+      Zdummy = 1;
+      Tdummy = 1;
+
+      hdf5write(OutFile, '/x_coords', Xdummy, 'WriteMode', 'append');
+      hdf5write(OutFile, '/y_coords', Ydummy, 'WriteMode', 'append');
+      hdf5write(OutFile, '/z_coords', Zdummy, 'WriteMode', 'append');
+      hdf5write(OutFile, '/t_coords', Tdummy, 'WriteMode', 'append');
+
+      fprintf('\n');
+    end
+    fprintf('\n');
+  end
+end
