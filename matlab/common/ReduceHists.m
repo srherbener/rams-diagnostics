9bda1c6ba4b4feb3f67be0d5646551d542f1df5d
steve <sherbener@atmos.colostate.edu>
2014-12-18 16:41:28 -0700

    Added percentile method of reducing histograms to a single point.

--- matlab/common/ReduceHists.m
+++ matlab/common/ReduceHists.m
@@ -1,12 +1,62 @@
-function [ Hreduced ] = ReduceHists( Hdata, Hdim, Bins, Method )
+function [ Hreduced ] = ReduceHists( Hdata, Hdim, Bins, Method, Ptile )
 % ReduceHists reduces histogram bins to a single number
 %
 % Method: 'wtmean' - weighted mean
 %         'max'    - max
 %         'com'    - center of mass
+%         'pct'    - Percentile of bin values corresponding
+%                    to the non-zero counts, percentile is
+%                    specified in Ptile argument.
+%
+
+
+% Create an array the is the same size as Hdata and contains the
+% bin values going down the dimension with the histogram counts.
+% This array will be used by the wtmean and pct90 methods.
+%
+% The bin value array (BINS) can be formed from an array that has
+% the same number of dimensions as Hdata with all dimension sizes
+% equal to one except for the dimension with the histogram counts, which has
+% its size set to the same size in Hdata. Note that this array will
+% have the same total length as the Bins array and by copying in
+% the Bins array into this array will automatically place one copy
+% of the bin values running along the dimension that corresponds to
+% that of the histogram counts in Hdata. Then repmat can be used to
+% tile the bin values into the same size of Hdata.
+
+Hsize = size(Hdata);
+
+% dimension sizes for array with all dims sizes equal to one
+% except for the dimension with the histograms
+InitSize = ones([ 1 length(Hsize) ]);
+InitSize(Hdim) = Hsize(Hdim);
+
+% dimesions sized for tiling with repmat where all dim sizes match
+% those of Hdata except for the dimension we placed the bin values
+% (which has a repeat factor of 1).
+RepFactor = Hsize;
+RepFactor(Hdim) = 1;
+
+BINS = ones(InitSize);
+BINS(:) = Bins;
+BINS = repmat(BINS, RepFactor);
+
+% BINS now is now the same size as Hdata with the bin values
+% running down the same dimension as the histogram counts.
 %
 
 switch Method
+    case 'pct'
+        % 90th percentile of the bin values corresponding to the non-zero
+        % histogram counts.
+        %
+        % Copy the Bins array into a new array. Set the locations where
+        % the counts (Hdata) has zeros to nans. Then run the prctile()
+        % command to get the 90th percentile of the bin value distributions.
+        BINS_WITH_COUNTS = BINS;
+        BINS_WITH_COUNTS(Hdata == 0) = nan;
+        Hreduced = squeeze(prctile(BINS_WITH_COUNTS, [ Ptile ], Hdim));
+
     case 'wtmean'
         % Weighted mean
         
@@ -22,36 +72,6 @@ switch Method
         % the element by element multiply will be way faster than writing a
         % loop to walk through each element of Hdata.)
         %
-        % The bin value array (BINS) can be formed from an array that has
-        % the same dimensions as Hdata with all dimension sizes equal to
-        % one except for the dimension with the histogram counts, which has
-        % its size set to the same size in Hdata. Note that this array will
-        % have the same total length as the Bins array and by copying in
-        % the Bins array into this array will automatically place one copy
-        % of the bin values running along the dimension that corresponds to
-        % that of the histogram counts in Hdata. The repmat can be used to
-        % tile the bin values into the same size of Hdata.
-        
-        Hsize = size(Hdata);
-
-        % dimension sizes for array with all dims sizes equal to one
-        % except for the dimension with the histograms
-        InitSize = ones([ 1 length(Hsize) ]);
-        InitSize(Hdim) = Hsize(Hdim);
-
-        % dimesions sized for tiling with repmat where all dim sizes match
-        % those of Hdata except for the dimension we placed the bin values
-        % (which has a repeat factor of 1).
-        RepFactor = Hsize;
-        RepFactor(Hdim) = 1;
-
-        BINS = ones(InitSize);
-        BINS(:) = Bins;
-        BINS = repmat(BINS, RepFactor);
-        
-        % BINS now is now the same size as Hdata with the bin values
-        % running down the same dimension as the histogram counts.
-        %
         % It's possible to get all counts equal to zero since we can filter
         % out input data before making the counts. In this case both SUM_BC
         % and SUM_C will be zero and we will proceed to do a 0/0 operation.
@@ -64,41 +84,16 @@ switch Method
     case 'max'
         % Max value
         %
-        % Create an array that contains ones where there are non zero
-        % counts in the corresponding histogram, and nans where all bins
-        % in the corresponding historgram contain zero counts. This array
-        % can then be multiplied by the the result of the max calculations
-        % in order to place nans where ever the histograms of all
-        % zero counts exist.
-        HAVE_COUNTS = squeeze(sum(Hdata, Hdim));
-        HAVE_COUNTS(HAVE_COUNTS ~= 0) = 1.0;
-        HAVE_COUNTS(HAVE_COUNTS == 0) = nan; 
- 
-        % Find the indices of the max values along the bins dimension (number 2)
-        % Then use that result to create an array organized as (r,z,t) that
-        % contains the bin value corresponding to the index where the max count
+        % Find the indices of the max values along the bins dimension.
+        % Then use that result to create an array that contains the bin
+        % value corresponding to the index where the max count
         % existed.
         [ MAXC_V, MAXC_I ] = max(Hdata, [], Hdim);
         Hreduced = squeeze(Bins(MAXC_I));
-        if (size(HAVE_COUNTS) == size(Hreduced'))
-          % catch the case where you have a column vector and a row vector
-          Hreduced = Hreduced' .* HAVE_COUNTS;
-        else
-          Hreduced = Hreduced .* HAVE_COUNTS;
-        end
+
     case 'com'
         % Center of mass
         %
-        % Create an array that contains ones where there are non zero
-        % counts in the corresponding histogram, and nans where all bins
-        % in the corresponding historgram contain zero counts. This array
-        % can then be multiplied by the the result of the max calculations
-        % in order to place nans where ever the histograms of all
-        % zero counts exist.
-        HAVE_COUNTS = squeeze(sum(Hdata, Hdim));
-        HAVE_COUNTS(HAVE_COUNTS ~= 0) = 1.0;
-        HAVE_COUNTS(HAVE_COUNTS == 0) = nan;
-        
         % For every histogram (along dimension Hdim) find the index nearest where
         % the area under the curve is half the total area under the curve. Then
         % use these indices to place the corresponding bin values into the result.
@@ -123,13 +118,7 @@ switch Method
         CSHALF = abs(CSUM - (SUM/2));
         [ HALF_V, HALF_I ] = min(CSHALF, [], Hdim);
         Hreduced = squeeze(Bins(HALF_I));
-        if (size(HAVE_COUNTS) == size(Hreduced'))
-          % catch the case where you have a column vector and a row vector
-          Hreduced = Hreduced' .* HAVE_COUNTS;
-        else
-          Hreduced = Hreduced .* HAVE_COUNTS;
-        end
-        
+
     otherwise
         fprintf('WARNING: ReduceHists: Unrecognized reduction method: %s\n', Method);
         fprintf('WARNING:              Setting output to all nans\n');
