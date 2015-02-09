1666fe5efd3108687bb5a3ebb4433b51ac50e559
steve <sherbener@atmos.colostate.edu>
2015-02-09 13:24:19 -0700

    Modified LocateSal.m so that it can be run on any grid. Put in lat/lon limits so that a reasonable SAL location in grid1 could be made.

--- matlab/ts_debby/LocateSal.m
+++ matlab/ts_debby/LocateSal.m
@@ -1,4 +1,4 @@
-function [ ] = LocateSal(RhLimit, Zmin, Zmax, ColCount, OutFile)
+function [ ] = LocateSal(InFile, InVname, RhLimit, LonRange, LatRange, Zrange, ColCount, OutFile)
 % LocateSal function to determine location of SAL within the horizontal domain
 %
 % This function will look at RH in layers between 1 km and 5 km AGL, determine whether or not the
@@ -8,7 +8,7 @@ function [ ] = LocateSal(RhLimit, Zmin, Zmax, ColCount, OutFile)
 % RhLimit is the relative humidity value (in percent) that specifies what constitutes the SAL
 % dry layer (RH < RhLimit).
 %
-% Zmin, Zmax mark the heights (in km) to look for the dry SAL layer, ie. only check RH from
+% Zrange ([Zmin, Zmax]) mark the heights (in km) to look for the dry SAL layer, ie. only check RH from
 % Zmin to Zmax heights.
 %
 % ColCount is the number of occurrences of RH < RhLimit required before that column is counted as
@@ -16,24 +16,35 @@ function [ ] = LocateSal(RhLimit, Zmin, Zmax, ColCount, OutFile)
 %
 % OutFile is the name of the output HDF5 file.
 
-%Case = 'TSD_3GRIDS';
-%Case = 'TSD_DRY_NODUST';
-Case = 'TSD_DRY_DUST';
+LonMin = LonRange(1);
+LonMax = LonRange(2);
 
-% Hfile is relative humidity (RH) at the first time step of the simulation
-Hfile = sprintf('HDF5/relhum_init-%s-AS-2006-08-20-120000-g3.h5', Case);
-Hdset = 'relhum';
+LatMin = LatRange(1);
+LatMax = LatRange(2);
+
+Zmin = Zrange(1);
+Zmax = Zrange(2);
 
 % RH will be organized as (x,y,z)
-fprintf('Reading: %s\n', Hfile);
-fprintf('  Dataset: %s\n', Hdset);
+fprintf('Reading: %s\n', InFile);
+fprintf('  Dataset: %s\n', InVname);
 fprintf('\n');
-RH = squeeze(hdf5read(Hfile, Hdset));
-X = squeeze(hdf5read(Hfile, '/x_coords'));      % deg lon
-Y = squeeze(hdf5read(Hfile, '/y_coords'));      % deg lat
-Z = squeeze(hdf5read(Hfile, '/z_coords'))/1000; % km AGL
+RH = squeeze(hdf5read(InFile, InVname));
+X = squeeze(hdf5read(InFile, '/x_coords'));      % deg lon
+Y = squeeze(hdf5read(InFile, '/y_coords'));      % deg lat
+Z = squeeze(hdf5read(InFile, '/z_coords'))/1000; % km AGL
 
-% Create a mask (1's and 0's) that shows where the SAL is based on anywhere
+Nx = length(X);
+Ny = length(Y);
+
+% We want the output to cover the entire lat/lon extent, but want
+% to confine the 1's in the output to the box defined by LonRange
+% and LatRange. Form a mask that has 1's inside the box and 0's outside.
+LON_MASK = repmat((X >= LonMin & X <= LonMax), [ 1 Ny ]);
+LAT_MASK = repmat((Y >= LatMin & Y <= LatMax)', [ Nx 1 ]);
+AREA_MASK = LON_MASK & LAT_MASK;
+
+% Create another mask (1's and 0's) that shows where the SAL is based on anywhere
 % in the column between 1km and 5km AGL that has RH < RhLimit.
 Z1 = find(Z >= Zmin, 1, 'first');
 Z2 = find(Z <= Zmax, 1, 'last');
@@ -41,7 +52,7 @@ Z2 = find(Z <= Zmax, 1, 'last');
 MASK = RH(:,:,Z1:Z2) < RhLimit;  % 0's where RH >= RhLimit, 1's where RH < RhLimit
 MASK = squeeze(sum(MASK,3));     % 0 where RH >= RhLimit, n where RH < RhLimit n times in the column
 
-MASK = double(MASK >= ColCount);  % have to have at least ColCount occurrences of RH < RhLimit in the column
+MASK = double((MASK >= ColCount) & AREA_MASK);  % have to have at least ColCount occurrences of RH < RhLimit in the column
 
 % Write out the mask
 fprintf('Writing: %s\n', OutFile);
