70e54c72f46b78371161fae1f9be070336c29c6e
steve <sherbener@atmos.colostate.edu>
2015-03-06 16:09:42 -0700

    PlotMaxWindAll and PlotMinPressAll can now accommodate variable lenght time series.

--- matlab/ts_debby/PlotMaxWindAll.m
+++ matlab/ts_debby/PlotMaxWindAll.m
@@ -10,40 +10,36 @@ function [ ] = PlotMaxWindAll(ConfigFile)
   end
 
   Cases = {
-   'TSD_DRY_DUST'
-   'TSD_DRY_NODUST'
-   'TSD_MOIST_DUST'
-   'TSD_MOIST_NODUST'
+%   'TSD_DRY_DUST'
+%   'TSD_DRY_NODUST'
+%   'TSD_MOIST_DUST'
+%   'TSD_MOIST_NODUST'
+
+   'TSD_SAL_DUST'
+   'TSD_SAL_NODUST'
    };
+  Nc = length(Cases);
 
   LegText = {
-%   'NHC Best Track'
-   'DD'
-   'DN'
-   'MD'
-   'MN'
+   'NHC Best Track'
+%   'DD'
+%   'DN'
+%   'MD'
+%   'MN'
+
+   'SAL\_DUST'
+   'SAL\_NODUST'
    };
 
-  % read in the Vt data
-  Nc = length(Cases);
+%  InFprefix = 'max_speed10m';
+%  InVname = '/max_speed10m';
+   InFprefix = 'max_speed25m';
+   InVname = '/max_speed25m';
+
   for icase = 1:Nc
-    %Hfile = sprintf('%s/max_speed25m_%s.h5', Tdir, Cases{icase});
-    %Hdset = '/max_speed25m';
-    Hfile = sprintf('%s/max_speed10m_%s.h5', Tdir, Cases{icase});
-    Hdset = '/max_speed10m';
-    fprintf('Reading: %s (%s)\n', Hfile, Hdset);
-
-    % Read in time series, S will be a column vector
-    S = squeeze(h5read(Hfile, Hdset));
-
-    if (icase == 1)
-      Nt = length(S);
-      TIMES = (squeeze(h5read(Hfile, '/t_coords')) ./3600) - 42.0; % hr
-      SPEED_T = zeros([ Nt Nc ]);
-    end
-    SPEED_T(:,icase) = S;
+    Hfile = sprintf('%s/%s_%s.h5', Tdir, InFprefix, Cases{icase});
+    InFiles{icase} = Hfile;
   end
-
 
   % NHC Best Track (every six hours) data
   % time step 1 from the simulation is where the NHC data starts
@@ -51,6 +47,31 @@ function [ ] = PlotMaxWindAll(ConfigFile)
   %
   NHC_WIND  = [ 35 35 35 40 45 50 50 50 50 50 50 ] * 0.447;
   NHC_TIMES = (0:6:60);
+
+  % first find the max number of time steps
+  MaxNt = 0;
+  for icase = 1:Nc
+     Hfile = InFiles{icase};
+     Hinfo = h5info(Hfile, '/t_coords');
+     Tsize = Hinfo.Dataspace.Size;
+     if (Tsize > MaxNt)
+       TIMES = (squeeze(h5read(Hfile, '/t_coords')) ./3600) - 42.0; % hr
+       MaxNt = Tsize;
+     end 
+  end
+
+  SPEED_T = nan([ MaxNt Nc ]);
+
+  % read in the Vt data
+  for icase = 1:Nc
+    Hfile = InFiles{icase};
+    fprintf('Reading: %s (%s)\n', Hfile, InVname);
+
+    % Read in time series, S will be a column vector
+    S = squeeze(h5read(Hfile, InVname));
+    Nt = length(S);
+    SPEED_T(1:Nt,icase) = S;
+  end
 
   % plot
   OutFile = sprintf('%s/TsDebbyWindAll.jpg', Pdir);
@@ -67,7 +88,7 @@ function [ ] = PlotMaxWindAll(ConfigFile)
   SimST = plot(TIMES, SPEED_T, 'LineWidth', LineW);
   set(gca, 'FontSize', Fsize);
   hold on;
-%  NhcST = plot(NHC_TIMES, NHC_WIND, '+k', 'LineWidth',  LineW);
+  NhcST = plot(NHC_TIMES, NHC_WIND, '+k', 'LineWidth',  LineW);
   %title('Maximum 10m Wind Speed');
   xlabel('Time');
   xlim (Xrange);
@@ -76,8 +97,8 @@ function [ ] = PlotMaxWindAll(ConfigFile)
   ylabel('Wind Speed (m s^-^1)');
   ylim(Yrange);
 
-%  legend([ NhcST SimST' ], LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
-  legend(SimST, LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
+  legend([ NhcST SimST' ], LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
+%  legend(SimST, LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
   
   fprintf('Writing: %s\n', OutFile);
   saveas(FigWind, OutFile);
--- matlab/ts_debby/PlotMinPressAll.m
+++ matlab/ts_debby/PlotMinPressAll.m
@@ -10,46 +10,68 @@ function [ ] = PlotMinPressAll(ConfigFile)
   end
 
   Cases = {
-   'TSD_DRY_DUST'
-   'TSD_DRY_NODUST'
-   'TSD_MOIST_DUST'
-   'TSD_MOIST_NODUST'
+%   'TSD_DRY_DUST'
+%   'TSD_DRY_NODUST'
+%   'TSD_MOIST_DUST'
+%   'TSD_MOIST_NODUST'
+
+   'TSD_SAL_DUST'
+   'TSD_SAL_NODUST'
    };
+  Nc = length(Cases);
 
   LegText = {
-%   'NHC Best Track'
-   'DD'
-   'DN'
-   'MD'
-   'MN'
+   'NHC Best Track'
+%   'DD'
+%   'DN'
+%   'MD'
+%   'MN'
+
+   'SAL\_DUST'
+   'SAL\_NODUST'
    };
 
-  % read in the Vt data
-  Nc = length(Cases);
+%  InFprefix = 'min_sea_press';
+%  InVname = '/min_sea_press';
+   InFprefix = 'min_press25m';
+   InVname = '/min_press25m';
+
   for icase = 1:Nc
-    %Hfile = sprintf('%s/min_press25m_%s.h5', Tdir, Cases{icase});
-    %Hdset = '/min_press25m';
-    Hfile = sprintf('%s/min_sea_press_%s.h5', Tdir, Cases{icase});
-    Hdset = '/min_sea_press';
-    fprintf('Reading: %s (%s)\n', Hfile, Hdset);
-
-    % Read in time series, P will be a column vector
-    P = squeeze(h5read(Hfile, Hdset));
-
-    if (icase == 1)
-      Nt = length(P);
-      TIMES = (squeeze(h5read(Hfile, '/t_coords')) ./3600) - 42.0; % hr
-      PRESS = zeros([ Nt Nc ]);
-    end
-    PRESS(:,icase) = P;
+    Hfile = sprintf('%s/%s_%s.h5', Tdir, InFprefix, Cases{icase});
+    InFiles{icase} = Hfile;
   end
-
 
   % NHC Best Track (every six hours) data
   % time step 1 from the simulation is where the NHC data starts
   %
   NHC_PRESS = [ 1007 1007 1005 1003 1002 1001 1001 1000  999 1000 1000 ];
   NHC_TIMES = (0:6:60);
+
+  % first find the max number of time steps
+  MaxNt = 0;
+  for icase = 1:Nc
+     Hfile = InFiles{icase};
+     Hinfo = h5info(Hfile, '/t_coords');
+     Tsize = Hinfo.Dataspace.Size;
+     if (Tsize > MaxNt)
+       TIMES = (squeeze(h5read(Hfile, '/t_coords')) ./3600) - 42.0; % hr
+       MaxNt = Tsize;
+     end 
+  end 
+
+  PRESS = nan([ MaxNt Nc ]);
+
+  % read in the Vt data
+  for icase = 1:Nc
+    Hfile = InFiles{icase};
+    fprintf('Reading: %s (%s)\n', Hfile, InVname);
+
+    % Read in time series, P will be a column vector
+    P = squeeze(h5read(Hfile, InVname));
+    Nt = length(P);
+
+    PRESS(1:Nt,icase) = P;
+  end
 
   % plot
   OutFile = sprintf('%s/TsDebbyPressAll.jpg', Pdir);
@@ -66,7 +88,7 @@ function [ ] = PlotMinPressAll(ConfigFile)
   SimST = plot(TIMES, PRESS, 'LineWidth', LineW);
   set(gca, 'FontSize', Fsize);
   hold on;
-%  NhcST = plot(NHC_TIMES, NHC_PRESS, '+k', 'LineWidth',  LineW);
+  NhcST = plot(NHC_TIMES, NHC_PRESS, '+k', 'LineWidth',  LineW);
   %title('Minimum SLP');
   xlabel('Time');
   xlim (Xrange);
@@ -75,8 +97,8 @@ function [ ] = PlotMinPressAll(ConfigFile)
   ylabel('Pressure (mb)');
   ylim(Yrange);
 
-%  legend([ NhcST SimST' ], LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
-  legend(SimST, LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
+  legend([ NhcST SimST' ], LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
+%  legend(SimST, LegText, 'Location', 'SouthEast', 'FontSize', LegendFsize);
   
   fprintf('Writing: %s\n', OutFile);
   saveas(FigWind, OutFile);
