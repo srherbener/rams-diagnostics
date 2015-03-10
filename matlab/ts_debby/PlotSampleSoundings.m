fe9eaffaaff0d9025da567ac02f626e17d63e048
steve <sherbener@atmos.colostate.edu>
2015-03-10 11:53:14 -0600

    Modified to plot out SAL and non-SAL soundings on same panel.

--- matlab/ts_debby/PlotSampleSoundings.m
+++ matlab/ts_debby/PlotSampleSoundings.m
@@ -10,92 +10,56 @@ function [ ] = PlotSmapleSoundings(ConfigFile)
   OutFile = sprintf('%s/SampleSoundings.jpg', Pdir);
 
   % Sample region, in the SAL location
-  X1 = 625;
-  X2 = 635;
-  %X1 = 630;
-  %X2 = 630;
+  X1 = 290;
+  X2 = 300;
 
-  Y1 = 535;
-  Y2 = 545;
-  %Y1 = 540;
-  %Y2 = 540;
-
-  %T1 = 30;
-  %T2 = 35;
-  T1 = 1;
-  T2 = 39;
+  Y1 = 385;
+  Y2 = 395;
 
   fprintf('****************** Creating sample soundings plot ******************\n');
 
-  % ************************** Read in the DRY case ***********************************
-  TempFile = 'HDF5/tempc-TSD_DRY_DUST-AS-2006-08-20-120000-g3.h5';
-  DewPtFile = 'HDF5/dewptc-TSD_DRY_DUST-AS-2006-08-20-120000-g3.h5';
+  % ************************** Read in the SAL case ***********************************
+  SalTempFile     = 'HDF5/tempc_init-TSD_SAL_DUST-AS-2006-08-20-120000-g3.h5';
+  SalDewPtFile    = 'HDF5/dewptc_init-TSD_SAL_DUST-AS-2006-08-20-120000-g3.h5';
+  NonSalTempFile  = 'HDF5/tempc_init-TSD_NONSAL_DUST-AS-2006-08-20-120000-g3.h5';
+  NonSalDewPtFile = 'HDF5/dewptc_init-TSD_NONSAL_DUST-AS-2006-08-20-120000-g3.h5';
 
-  TempVar = 'tempc';
-  DewPtVar = 'dewptc';
+  TempVar = '/tempc';
+  DewPtVar = '/dewptc';
 
-  fprintf('  DRY Case:\n');
-  fprintf('    Reading: %s (%s)\n', TempFile, TempVar);
-  fprintf('    Reading: %s (%s)\n', DewPtFile, DewPtVar);
+  fprintf('  SAL Case:\n');
+  fprintf('    Reading: %s (%s)\n', SalTempFile, TempVar);
+  fprintf('    Reading: %s (%s)\n', SalDewPtFile, DewPtVar);
+  fprintf('\n');
+  fprintf('  non-SAL Case:\n');
+  fprintf('    Reading: %s (%s)\n', NonSalTempFile, TempVar);
+  fprintf('    Reading: %s (%s)\n', NonSalDewPtFile, DewPtVar);
   fprintf('\n');
 
-  T_DS  = ncgeodataset(TempFile);
-  TD_DS = ncgeodataset(DewPtFile);
-
-  T_VAR  = T_DS.geovariable(TempVar);
-  TD_VAR = TD_DS.geovariable(DewPtVar);
-
-  Z_VAR = T_DS.geovariable('z_coords');
+  SAL_T   = squeeze(h5read(SalTempFile, TempVar));
+  SAL_TD  = squeeze(h5read(SalDewPtFile, DewPtVar));
+  NSAL_T  = squeeze(h5read(NonSalTempFile, TempVar));
+  NSAL_TD = squeeze(h5read(NonSalDewPtFile, DewPtVar));
 
   % Grab the z values, and set the z limits
-  Z = Z_VAR.data(:) ./1000;  % km
+  Z = squeeze(h5read(SalTempFile, '/z_coords')) ./1000;  % km
 
   Z1 = find(Z >= 0, 1, 'first');
   Z2 = find(Z <= 30, 1, 'last');
  
   Z = Z(Z1:Z2);
 
-  % Vars are organized as: (t,z,y,x)
-  TEMP  = T_VAR.data(1:end,Z1:Z2,Y1:Y2,X1:X2);
-  DEWPT = TD_VAR.data(1:end,Z1:Z2,Y1:Y2,X1:X2);
-
-  % Construct a spatial and temporal mean to produce a single sounding
-  TEMP_AVG = squeeze(mean(mean(mean(TEMP, 4), 3), 1))';
-  DEWPT_AVG = squeeze(mean(mean(mean(DEWPT, 4), 3), 1))';
-
-  DRY_PDATA = [ TEMP_AVG DEWPT_AVG ];
-
-  % ************************** Read in the MOIST case ***********************************
-  TempFile = 'HDF5/tempc-TSD_MOIST_NODUST-AS-2006-08-20-120000-g3.h5';
-  DewPtFile = 'HDF5/dewptc-TSD_MOIST_NODUST-AS-2006-08-20-120000-g3.h5';
-
-  TempVar = 'tempc';
-  DewPtVar = 'dewptc';
-
-  fprintf('  MOIST Case:\n');
-  fprintf('    Reading: %s (%s)\n', TempFile, TempVar);
-  fprintf('    Reading: %s (%s)\n', DewPtFile, DewPtVar);
-  fprintf('\n');
-
-  T_DS  = ncgeodataset(TempFile);
-  TD_DS = ncgeodataset(DewPtFile);
-
-  T_VAR  = T_DS.geovariable(TempVar);
-  TD_VAR = TD_DS.geovariable(DewPtVar);
-
-  % Vars are organized as: (t,z,y,x)
-  TEMP  = T_VAR.data(1:end,Z1:Z2,Y1:Y2,X1:X2);
-  DEWPT = TD_VAR.data(1:end,Z1:Z2,Y1:Y2,X1:X2);
-
-  % Construct a spatial and temporal mean to produce a single sounding
-  TEMP_AVG = squeeze(mean(mean(mean(TEMP, 4), 3), 1))';
-  DEWPT_AVG = squeeze(mean(mean(mean(DEWPT, 4), 3), 1))';
-
-  MOIST_PDATA = [ TEMP_AVG DEWPT_AVG ];
-
+  % Vars are organized as: (x,y,z)
+  % Construct a spatial mean to produce a single sounding
+  SAL_T_AVG   = squeeze(mean(mean(SAL_T(X1:X2,Y1:Y2,Z1:Z2), 2), 1));
+  SAL_TD_AVG  = squeeze(mean(mean(SAL_TD(X1:X2,Y1:Y2,Z1:Z2), 2), 1));
+  NSAL_T_AVG  = squeeze(mean(mean(NSAL_T(X1:X2,Y1:Y2,Z1:Z2), 2), 1));
+  NSAL_TD_AVG = squeeze(mean(mean(NSAL_TD(X1:X2,Y1:Y2,Z1:Z2), 2), 1));
 
   %******************************************************************************
-  % Two panel plot
+  % single panel plot
+  % put SAL lines in solid, NONSAL lines in dashed
+  % put T in red, Td in green
   Fig = figure;
 
   Xlab = 'T (^{\circ}C)';
@@ -103,6 +67,13 @@ function [ ] = PlotSmapleSoundings(ConfigFile)
 
   Xlimits = [ -100 50 ];
   Xticks = [ -50 0 50 ];
+
+  LegText = {
+    'SAL T'
+    'SAL T_d'
+    'non-SAL T'
+    'non-SAL T_d'
+    };
 
   % patch inputs for drawing a rectangle
   SalX = [ Xlimits(1) Xlimits(2) Xlimits(2) Xlimits(1) ]; % full x-axis range
@@ -117,39 +88,24 @@ function [ ] = PlotSmapleSoundings(ConfigFile)
   LegFsize = 15;
   LineWidth = 2;
 
-  % *** DRY ***
-  subplot(1,2,1);
-  plot(DRY_PDATA, Z, 'LineWidth', LineWidth);
+  plot(SAL_T_AVG, Z, 'LineWidth', LineWidth, 'Color', str2rgb('red'), 'LineStyle', '-');
+  hold on
+  plot(SAL_TD_AVG, Z, 'LineWidth', LineWidth, 'Color', str2rgb('green'), 'LineStyle', '-');
+  plot(NSAL_T_AVG, Z, 'LineWidth', LineWidth, 'Color', str2rgb('red'), 'LineStyle', '--');
+  plot(NSAL_TD_AVG, Z, 'LineWidth', LineWidth, 'Color', str2rgb('green'), 'LineStyle', '--');
+
   set (gca, 'FontSize', Fsize);
   xlim(Xlimits);
   set (gca, 'XTick', Xticks);
 
-  title('DRY');
   xlabel(Xlab);
   ylabel(Ylab);
 
-  % mark sal location
-  patch(SalX, SalY, SalColor, 'EdgeColor', 'none', 'FaceAlpha', 0.2);
-  text(SalLabX, SalLabY, 'SAL', 'FontSize', SalFsize);
-
-  % *** MOIST ***
-  subplot(1,2,2);
-  plot(MOIST_PDATA, Z, 'LineWidth', LineWidth);
-  set (gca, 'FontSize', Fsize);
-  xlim(Xlimits);
-  set (gca, 'XTick', Xticks);
-
-  % shut off labels on y-axis, will share with left panel
-  title('MOIST');
-  xlabel(Xlab);
-  set (gca, 'YTickLabel', []);
-
-  legend({ 'Temp.' 'Dew Pt.' }, 'FontSize', LegFsize);
+  legend(LegText, 'FontSize', LegFsize);
 
   % mark sal location
   patch(SalX, SalY, SalColor, 'EdgeColor', 'none', 'FaceAlpha', 0.2);
   text(SalLabX, SalLabY, 'SAL', 'FontSize', SalFsize);
-  
 
   fprintf('  Writing: %s\n', OutFile);
   saveas(Fig, OutFile);
