e07974698e796a116963fe14c89b43f859711ebc
steve <sherbener@atmos.colostate.edu>
2016-03-01 13:58:52 -0700

    plot time series of total integrated mass for the vertical ranging from the surface to the tropopause (16.5 km).

--- matlab/ts_debby/PlotDpFigSfcTropTseries.m
+++ matlab/ts_debby/PlotDpFigSfcTropTseries.m
@@ -1,5 +1,5 @@
-function [ ] = PlotDpFigDustRemoval()
-% PlotDpFigDustRemoval 
+function [ ] = PlotDpFigSfcTropTseries()
+% PlotDpFigDustTransport 
 
   Pdir = 'Plots';
   if (exist(Pdir, 'dir') ~= 7)
@@ -8,55 +8,55 @@ function [ ] = PlotDpFigDustRemoval()
 
   Fsize = 13;
   
-  % Hovmoller data for dust-in-hydrometeors
-  InFile = 'DIAGS/storm_hovmollers_TSD_SAL_DUST.h5';
-  InVname = '/sal_dust_hydro';
-  fprintf('Reading: %s (%s)\n', InFile, InVname);
-
-  HOV_DUST = squeeze(h5read(InFile, InVname));
-  Z    = squeeze(h5read(InFile, '/z_coords'))./1000;
-  T    = squeeze(h5read(InFile, '/t_coords'))./3600 - 42;
-
-  % Time series of lower level Mdhy
+  % Time series of upper level Mdhy
   InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
-  InVname = '/sal_dust_hydro_total_mass_llev';
+  InVname = '/sal_dust_hydro_total_mass';
   fprintf('Reading: %s (%s)\n', InFile, InVname);
 
   TS_SAL_MDHY = squeeze(h5read(InFile, InVname)); % g
-  TS_SAL_MDHY = TS_SAL_MDHY .* 1e-9;  % convert to 1e-3 Tg
+  TS_SAL_MDHY = TS_SAL_MDHY .* 1e-9;  % convert 1e-3 Tg
 
-  % Time series of precip rate
-  InFile = 'DIAGS/hist_meas_ts_pcprate_TSD_SAL_DUST.h5';
-  InVname = '/sal_pcprate_ts';
-  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  T    = squeeze(h5read(InFile, '/t_coords'))./3600 -42;
 
-  TS_SAL_PR = squeeze(h5read(InFile, InVname)); % mm/h
-
-  % Time series of surface dust accumulation
+  % Time series of upper level Mdregen
   InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
-  InVname = '/sal_dust_sfc_total_mass';
+  InVname = '/sal_ra_total_mass';
   fprintf('Reading: %s (%s)\n', InFile, InVname);
 
-  TS_SAL_DSFC = squeeze(h5read(InFile, InVname)); % g
-  TS_SAL_DSFC = TS_SAL_DSFC .* 1e-12;  % convert to Tg
+  TS_SAL_MDREGEN = squeeze(h5read(InFile, InVname)); % g
+  TS_SAL_MDREGEN = TS_SAL_MDREGEN .* 1e-12;  % convert Tg
 
-  % plot, 4 panels (2x2)
-  OutFile = sprintf('%s/DpFigDustRemoval.jpg', Pdir);
-  
+  % Time series of upper level Md1
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_d1_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MD1 = squeeze(h5read(InFile, InVname)); % g
+  TS_SAL_MD1 = TS_SAL_MD1 .* 1e-9;  % convert 1e-3 Tg
+
+  % Time series of upper level Md2
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_d2_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MD2 = squeeze(h5read(InFile, InVname)); % g
+  TS_SAL_MD2 = TS_SAL_MD2 .* 1e-12;  % convert Tg
+
+  % all on one plot
+  OutFile = sprintf('%s/DpFigDustSfcTrop.jpg', Pdir);
   Fig = figure;
 
-  % create the Hovmoller of sal_dust_hydro
   Paxes = subplot(4,1,1);
-  PlotDpFigDustHov(Paxes, T, Z, HOV_DUST, 'a', 'SAL\_AR: M_d_h_y', Fsize, 0, 1, 0, 0);
-  
+  PlotDpFigTseries(Paxes, T, TS_SAL_MD1, 'a', 'SAL\_AR', 'M_d_1 (10^-^3 Tg)', Fsize, 0, 1, { }, 'none');
+
   Paxes = subplot(4,1,2);
-  PlotDpFigTseries(Paxes, T, TS_SAL_MDHY, 'b', 'SAL\_AR', 'M_d_h_y (10^-^3 Tg)', Fsize, 0, 1, { }, 'none');
+  PlotDpFigTseries(Paxes, T, TS_SAL_MD2, 'b', 'SAL\_AR', 'M_d_2 (Tg)', Fsize, 0, 1, { }, 'none');
 
   Paxes = subplot(4,1,3);
-  PlotDpFigTseries(Paxes, T, TS_SAL_PR, 'c', 'SAL\_AR', 'PR (mm h^-^1)', Fsize, 0, 1, { }, 'none');
+  PlotDpFigTseries(Paxes, T, TS_SAL_MDHY, 'c', 'SAL\_AR', 'M_d_h_y (10^-^3 Tg)', Fsize, 0, 1, { }, 'none');
 
   Paxes = subplot(4,1,4);
-  PlotDpFigTseries(Paxes, T, TS_SAL_DSFC, 'd', 'SAL\_AR', 'M_d_s_f_c (Tg)', Fsize, 1, 1, { }, 'none');
+  PlotDpFigTseries(Paxes, T, TS_SAL_MDREGEN, 'd', 'SAL\_AR', 'M_d_r_g (Tg)', Fsize, 1, 1, { }, 'none');
 
   fprintf('Writing: %s\n', OutFile);
   saveas(Fig, OutFile);
