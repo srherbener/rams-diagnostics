276af522477d86e15285bbcee6374309d70d2206
steve <sherbener@atmos.colostate.edu>
2016-03-02 11:20:05 -0700

    Added sal_ar, spath and storm to the residual calculations. New script to report the start, end and difference values of total mass.

--- matlab/ts_debby/GenResidualMassMeas.m
+++ matlab/ts_debby/GenResidualMassMeas.m
@@ -10,6 +10,15 @@ function [ ] = GenResidualMassMeas()
     };
   Ncases = length(CaseList);
 
+  % Description of measurements
+  FileList = {
+    { 'DIAGS/total_mass_<CASE>.h5' 'sal'    }
+    { 'DIAGS/total_mass_<CASE>.h5' 'sal_ar' }
+    { 'DIAGS/total_mass_<CASE>.h5' 'spath'  }
+    { 'DIAGS/total_mass_<CASE>.h5' 'storm'  }
+    };
+  Nfiles = length(FileList);
+
   for icase = 1:Ncases
     Case = CaseList{icase};
 
@@ -26,91 +35,98 @@ function [ ] = GenResidualMassMeas()
       delete(OutFile);
     end
 
-    InFile  = sprintf('DIAGS/total_mass_%s.h5', Case);
+    for ifile = 1:Nfiles
+      Ftemplate = FileList{ifile}{1};
+      Vprefix   = FileList{ifile}{2};
 
-    % Total unactivated dust mass, all levels
-    InVname = '/sal_dust_total_mass';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MD = squeeze(h5read(InFile, InVname));
-
-    X = squeeze(h5read(InFile, '/x_coords'));
-    Y = squeeze(h5read(InFile, '/y_coords'));
-    Z = squeeze(h5read(InFile, '/z_coords'));
-    T = squeeze(h5read(InFile, '/t_coords'));
-
-    Nx = length(X);
-    Ny = length(Y);
-    Nz = length(Z);
-    Nt = length(T);
-
-    % regenerated dust mass, all levels
-    InVname = '/sal_ra_total_mass';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MDRGN = squeeze(h5read(InFile, InVname));
-
-    % dust in hydrometeor mass, all levels
-    InVname = '/sal_dust_hydro_total_mass';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MDHY = squeeze(h5read(InFile, InVname));
-
-    % dust deposited on surface
-    InVname = '/sal_dust_sfc_total_mass';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MDSFC = squeeze(h5read(InFile, InVname));
-
-    fprintf('\n');
-
-
-    % Total unactivated dust mass, upper levels
-    InVname = '/sal_dust_total_mass_hlev';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MD_HLEV = squeeze(h5read(InFile, InVname));
-
-    % regenerated dust mass, upper levels
-    InVname = '/sal_ra_total_mass_hlev';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MDRGN_HLEV = squeeze(h5read(InFile, InVname));
-
-    % dust in hydrometeor mass, upper levels
-    InVname = '/sal_dust_hydro_total_mass_hlev';
-    fprintf('  Reading: %s (%s)\n', InFile, InVname);
-    TS_MDHY_HLEV = squeeze(h5read(InFile, InVname));
-
-    fprintf('\n');
-
-    % residual is = Md - (Mdrgn + Mdhy + Mdsfc)
-    % Mdsfc is only for sfc to tropopause integrated values
-    TS_MDRES = TS_MD - (TS_MDRGN + TS_MDHY + TS_MDSFC);
-    TS_MDRES_HLEV = TS_MD_HLEV - (TS_MDRGN_HLEV + TS_MDHY_HLEV);
-
-    % Write out results
-    Vsize = Nt;
-    DimOrder = { 't' };
-
-    % create coordinates
-    Xname = '/x_coords';
-    Yname = '/y_coords';
-    Zname = '/z_coords';
-    Tname = '/t_coords';
-
-    CreateDimensionsXyzt(OutFile, X, Y, Z, T, Xname, Yname, Zname, Tname);
-    % Add COARDS annotations
-    NotateDimensionsXyzt(OutFile, Xname, Yname, Zname, Tname);
-
-    % residual, all levels
-    OutVname = '/sal_residual_total_mass';
-    fprintf('  Writing: %s (%s)\n', OutFile, OutVname);
-    h5create(OutFile, OutVname, Vsize);
-    h5write(OutFile, OutVname, TS_MDRES);
-    AttachDimensionsXyzt(OutFile, OutVname, DimOrder, Xname, Yname, Zname, Tname);
-
-    % residual, upper levels
-    OutVname = '/sal_residual_total_mass_hlev';
-    fprintf('  Writing: %s (%s)\n', OutFile, OutVname);
-    h5create(OutFile, OutVname, Vsize);
-    h5write(OutFile, OutVname, TS_MDRES_HLEV);
-    AttachDimensionsXyzt(OutFile, OutVname, DimOrder, Xname, Yname, Zname, Tname);
-
-    fprintf('\n');
+      InFile  = regexprep(Ftemplate, '<CASE>', Case);
+  
+      % Total unactivated dust mass, all levels
+      InVname = sprintf('/%s_dust_total_mass', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MD = squeeze(h5read(InFile, InVname));
+  
+      X = squeeze(h5read(InFile, '/x_coords'));
+      Y = squeeze(h5read(InFile, '/y_coords'));
+      Z = squeeze(h5read(InFile, '/z_coords'));
+      T = squeeze(h5read(InFile, '/t_coords'));
+  
+      Nx = length(X);
+      Ny = length(Y);
+      Nz = length(Z);
+      Nt = length(T);
+  
+      % regenerated dust mass, all levels
+      InVname = sprintf('/%s_ra_total_mass', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MDRGN = squeeze(h5read(InFile, InVname));
+  
+      % dust in hydrometeor mass, all levels
+      InVname = sprintf('/%s_dust_hydro_total_mass', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MDHY = squeeze(h5read(InFile, InVname));
+  
+      % dust deposited on surface
+      InVname = sprintf('/%s_dust_sfc_total_mass', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MDSFC = squeeze(h5read(InFile, InVname));
+  
+      fprintf('\n');
+  
+  
+      % Total unactivated dust mass, upper levels
+      InVname = sprintf('/%s_dust_total_mass_hlev', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MD_HLEV = squeeze(h5read(InFile, InVname));
+  
+      % regenerated dust mass, upper levels
+      InVname = sprintf('/%s_ra_total_mass_hlev', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MDRGN_HLEV = squeeze(h5read(InFile, InVname));
+  
+      % dust in hydrometeor mass, upper levels
+      InVname = sprintf('/%s_dust_hydro_total_mass_hlev', Vprefix);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      TS_MDHY_HLEV = squeeze(h5read(InFile, InVname));
+  
+      fprintf('\n');
+  
+      % residual is = Md - (Mdrgn + Mdhy + Mdsfc)
+      % Mdsfc is only for sfc to tropopause integrated values
+      TS_MDRES = TS_MD - (TS_MDRGN + TS_MDHY + TS_MDSFC);
+      TS_MDRES_HLEV = TS_MD_HLEV - (TS_MDRGN_HLEV + TS_MDHY_HLEV);
+  
+      % Write out results
+      Vsize = Nt;
+      DimOrder = { 't' };
+  
+      if (ifile == 1)
+        % create coordinates
+        Xname = '/x_coords';
+        Yname = '/y_coords';
+        Zname = '/z_coords';
+        Tname = '/t_coords';
+  
+        CreateDimensionsXyzt(OutFile, X, Y, Z, T, Xname, Yname, Zname, Tname);
+        % Add COARDS annotations
+        NotateDimensionsXyzt(OutFile, Xname, Yname, Zname, Tname);
+      end
+  
+      % residual, all levels
+      OutVname = sprintf('/%s_residual_total_mass', Vprefix);
+      fprintf('  Writing: %s (%s)\n', OutFile, OutVname);
+      h5create(OutFile, OutVname, Vsize);
+      h5write(OutFile, OutVname, TS_MDRES);
+      AttachDimensionsXyzt(OutFile, OutVname, DimOrder, Xname, Yname, Zname, Tname);
+  
+      % residual, upper levels
+      OutVname = sprintf('/%s_residual_total_mass_hlev', Vprefix);
+      fprintf('  Writing: %s (%s)\n', OutFile, OutVname);
+      h5create(OutFile, OutVname, Vsize);
+      h5write(OutFile, OutVname, TS_MDRES_HLEV);
+      AttachDimensionsXyzt(OutFile, OutVname, DimOrder, Xname, Yname, Zname, Tname);
+  
+      fprintf('\n');
+    end
   end
 end 
---
+++ matlab/ts_debby/ReportIntMassNumbers.m
@@ -0,0 +1,89 @@
+function [ ] = ReportIntMassNumbers()
+% ReportIntMassNumbers function to report initial and final volume integrated mass numbers
+
+  % Cases
+  CaseList = {
+    'TSD_SAL_DUST'
+%    'TSD_SAL_NODUST'
+%    'TSD_NONSAL_DUST'
+%    'TSD_NONSAL_NODUST'
+    };
+  Ncases = length(CaseList);
+
+  % Description of measurements
+  FileList = {
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_dust_total_mass'       'SAL_AR (large), Unactivated:'          }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_dust_sfc_total_mass'   'SAL_AR (large), Deposited to surface:' }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_dust_hydro_total_mass' 'SAL_AR (large), Inside hydrometeors:'  }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_ra_total_mass'         'SAL_AR (large), Regenerated:'          }
+    { 'DIAGS/residual_mass_<CASE>.h5' '/sal_residual_total_mass'   'SAL_AR (large), Advected (residual):'  }
+
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_ar_dust_total_mass'       'SAL_AR (med), Unactivated:'          }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_ar_dust_sfc_total_mass'   'SAL_AR (med), Deposited to surface:' }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_ar_dust_hydro_total_mass' 'SAL_AR (med), Inside hydrometeors:'  }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/sal_ar_ra_total_mass'         'SAL_AR (med), Regenerated:'          }
+    { 'DIAGS/residual_mass_<CASE>.h5' '/sal_ar_residual_total_mass'   'SAL_AR (med), Advected (residual):'  }
+
+    { 'DIAGS/total_mass_<CASE>.h5'    '/spath_dust_total_mass'       'SPATH (small), Unactivated:'          }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/spath_dust_sfc_total_mass'   'SPATH (small), Deposited to surface:' }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/spath_dust_hydro_total_mass' 'SPATH (small), Inside hydrometeors:'  }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/spath_ra_total_mass'         'SPATH (small), Regenerated:'          }
+    { 'DIAGS/residual_mass_<CASE>.h5' '/spath_residual_total_mass'   'SPATH (small), Advected (residual):'  }
+
+    { 'DIAGS/total_mass_<CASE>.h5'    '/storm_dust_total_mass'       'STORM (500km), Unactivated:'          }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/storm_dust_sfc_total_mass'   'STORM (500km), Deposited to surface:' }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/storm_dust_hydro_total_mass' 'STORM (500km), Inside hydrometeors:'  }
+    { 'DIAGS/total_mass_<CASE>.h5'    '/storm_ra_total_mass'         'STORM (500km), Regenerated:'          }
+    { 'DIAGS/residual_mass_<CASE>.h5' '/storm_residual_total_mass'   'STORM (500km), Advected (residual):'  }
+    };
+  Nfiles = length(FileList);
+
+  for icase = 1:Ncases
+    Case = CaseList{icase};
+
+    fprintf('*****************************************************************\n');
+    fprintf('Integrated Mass Measurements:\n');
+    fprintf('  Case: %s\n', Case);
+    fprintf('\n');
+
+    % Run through entire file list first, record the numbers and dump out
+    % the report last. This way the report is all in one section instead of
+    % interspersed between the "Reading: ..." messages.
+
+    clear RDATA;
+    for ifile = 1:Nfiles
+      Ifile   = FileList{ifile}{1};
+      InVname = FileList{ifile}{2};
+      Descrip = FileList{ifile}{3};
+
+      % HDATA will be (t), and units are grams
+      InFile  = regexprep(Ifile, '<CASE>', Case);
+      fprintf('  Reading: %s (%s)\n', InFile, InVname);
+      HDATA = squeeze(h5read(InFile, InVname));
+
+      Mstart = HDATA(1)   * 1e-12; % Convert to Tg
+      Mend   = HDATA(end) * 1e-12; % Convert to Tg
+
+      Mdiff = Mend - Mstart;
+
+      % record the description, start value, end value and difference between start and end
+      RDATA{ifile} = { Descrip Mstart Mend Mdiff };
+    end
+    fprintf('\n');
+    fprintf('\n');
+
+    % Dump out report
+    OutFile = fopen('DIAGS/IntMassReport.txt', 'w');
+
+    fprintf(OutFile, '%-40s %10s %10s %10s\n', '        Mass Quantity', 'Start', 'End', 'Diff');
+    fprintf(OutFile, '\n');
+    for imeas = 1:length(RDATA)
+      Descrip = RDATA{imeas}{1};
+      Mstart  = RDATA{imeas}{2};
+      Mend    = RDATA{imeas}{3};
+      Mdiff   = RDATA{imeas}{4};
+      fprintf(OutFile, '  %-40s %10f %10f %10f \n', Descrip, Mstart, Mend, Mdiff);
+    end
+    fclose(OutFile);
+  end
+end 
