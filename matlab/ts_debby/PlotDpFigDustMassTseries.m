4af82b10372029afd0e92810cf9d8a69b497e210
steve <sherbener@atmos.colostate.edu>
2016-03-04 15:00:08 -0700

    Oops, forgot these.

---
+++ matlab/ts_debby/PlotDpFigDustMassTseries.m
@@ -0,0 +1,101 @@
+function [ ] = PlotDpFigDustMassTseries()
+% PlotDpFigDustRemoval 
+
+  Pdir = 'Plots';
+  if (exist(Pdir, 'dir') ~= 7)
+      mkdir(Pdir);
+  end
+
+  Fsize = 13;
+  
+
+  %%%%%%%%%%%%%%%% Volumn integrated dust - surface to tropopause %%%%%%%%%%%%%
+  % Time series of Md
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MD = squeeze(h5read(InFile, InVname)); % g
+  T    = squeeze(h5read(InFile, '/t_coords'))./3600 - 42;
+
+  % Time series of Mdhy
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_hydro_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MDHY = squeeze(h5read(InFile, InVname)); % g
+
+  % Time series of Mdrgn
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_ra_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MDRGN = squeeze(h5read(InFile, InVname)); % g
+
+  % Time series of Mdsfc
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_sfc_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MDSFC = squeeze(h5read(InFile, InVname)); % g
+
+  % Time series of Mdadv
+  InFile = 'DIAGS/residual_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_residual_total_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+
+  TS_SAL_MDADV = squeeze(h5read(InFile, InVname)); % g
+
+  TS_DUST_AL = [ TS_SAL_MD TS_SAL_MDHY TS_SAL_MDRGN TS_SAL_MDADV TS_SAL_MDSFC ] .* 1e-12; % convert to Tg
+  LegTextAl = { 'M_d' 'M_d_h_y' 'M_d_r_g_n' 'M_d_a_d_v' 'M_d_s_f_c' };
+  LegLocAl = 'NorthEastOutside';
+
+  fprintf('\n');
+
+  %%%%%%%%%%%%%%%% Volumn integrated dust - 7 km AGL to tropopause %%%%%%%%%%%%%
+  % Time series of upper level Md
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_total_mass_hlev';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  TS_SAL_MD = squeeze(h5read(InFile, InVname)); % g
+  T    = squeeze(h5read(InFile, '/t_coords'))./3600 -42;
+
+  % Time series of upper level Mdhy
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_hydro_total_mass_hlev';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  TS_SAL_MDHY = squeeze(h5read(InFile, InVname)); % g
+
+  % Time series of upper level Mdrgn
+  InFile = 'DIAGS/total_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_ra_total_mass_hlev';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  TS_SAL_MDRGN = squeeze(h5read(InFile, InVname)); % g
+
+  % Time series of upper level Mdadv
+  InFile = 'DIAGS/residual_mass_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_residual_total_mass_hlev';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  TS_SAL_MDADV = squeeze(h5read(InFile, InVname)); % g
+
+  TS_DUST_UL = [ TS_SAL_MD TS_SAL_MDHY TS_SAL_MDRGN TS_SAL_MDADV ] .* 1e-9; % convert to 1e-3 Tg
+  LegTextUl = { 'M_d' 'M_d_h_y' 'M_d_r_g_n' 'M_d_a_d_v' };
+  LegLocUl = 'NorthEastOutside';
+
+  fprintf('\n');
+
+  % plot, 4 panels (2x2)
+  OutFile = sprintf('%s/DpFig3_DustMassTseries.jpg', Pdir);
+  
+  Fig = figure;
+
+  Paxes = subplot(2,1,1);
+  PlotDpFigTseries(Paxes, T, TS_DUST_AL, 'a', 'All Levels', 'Mass (Tg)', 'log', [ 1e-4 1e1 ], Fsize, 0, 1, LegTextAl, LegLocAl);
+
+  Paxes = subplot(2,1,2);
+  PlotDpFigTseries(Paxes, T, TS_DUST_UL, 'a', 'Upper Levels', 'Mass (10^-^3 Tg)', 'linear', [ -0.5 3.5 ], Fsize, 1, 1, LegTextUl, LegLocUl);
+
+  fprintf('Writing: %s\n', OutFile);
+  saveas(Fig, OutFile);
+  close(Fig);
+end
---
+++ matlab/ts_debby/PlotDpFigHovmollers.m
@@ -0,0 +1,49 @@
+function [ ] = PlotDpFigHovmollers()
+
+  Pdir = 'Plots';
+  if (exist(Pdir, 'dir') ~= 7)
+      mkdir(Pdir);
+  end
+  
+  Fsize = 13;
+
+  % Hovmoller data for Md
+  InFile = 'DIAGS/storm_hovmollers_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  HOV_DUST = squeeze(h5read(InFile, InVname));
+  Z    = squeeze(h5read(InFile, '/z_coords'))./1000;         % convert to km
+  T    = squeeze(h5read(InFile, '/t_coords'))./3600 - 42;    % convert to sim time in hours
+
+  % Hovmoller data for Mdhy
+  InFile = 'DIAGS/storm_hovmollers_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_dust_hydro';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  HOV_DHY = squeeze(h5read(InFile, InVname));
+
+  % Hovmoller data for Mdrgn
+  InFile = 'DIAGS/storm_hovmollers_TSD_SAL_DUST.h5';
+  InVname = '/sal_ar_ra_mass';
+  fprintf('Reading: %s (%s)\n', InFile, InVname);
+  HOV_DRGN = squeeze(h5read(InFile, InVname));
+
+  % plot
+  Fig = figure;
+
+  % Md
+  Paxes = subplot(3,1,1);
+  PlotDpFigDustHov(Paxes, T, Z, HOV_DUST, 'a', 'SAL\_AR: M_d', Fsize, 0, 1, 0, 2);
+
+  % Mdhy
+  Paxes = subplot(3,1,2);
+  PlotDpFigDustHov(Paxes, T, Z, HOV_DHY, 'b', 'SAL\_AR: M_d_h_y', Fsize, 0, 1, 0, 0);
+
+  % Mdrgn
+  Paxes = subplot(3,1,3);
+  PlotDpFigDustHov(Paxes, T, Z, HOV_DRGN, 'c', 'SAL\_AR: M_d_r_g_n', Fsize, 1, 1, 0, 1);
+  
+  OutFile = sprintf('%s/DpFig2_Hovmollers.jpg', Pdir);
+  fprintf('Writing: %s\n', OutFile);
+  saveas(Fig, OutFile);
+  close(Fig);
+end
