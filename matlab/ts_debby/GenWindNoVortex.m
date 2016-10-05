39c5a795d23595f555aaebb8afa5969044d56292
steve <sherbener@atmos.colostate.edu>
2016-10-05 15:34:21 -0600

    Progress on the Kurihara et al. (1993) vortex separation method. Got The basic field extraction working.

--- matlab/ts_debby/GenWindNoVortex.m
+++ matlab/ts_debby/GenWindNoVortex.m
@@ -16,8 +16,16 @@ function [ ] = GenWindNoVortex()
 %        a) azimuthal average
 %    4) subtract out circulation from vortex region
 %
+%  Method == 3
+%    Vortex separation as described in Kurihara et al. (1995, 1993)
+%    1) Separate the total wind field into a basic field and
+%       disturbance field. Use the smoothing method in Kurihara
+%       et al. (1993) to do this.
+%         a) Kurihara et al. (1995) says to use sigma = 0.85 level
+%            which is rougly 1500 m elevation. Say p0 = 1000 mb, then
+%            sigma = 0.85 is 850 mb which is roughly 1500 m elevation
 
-  Method = 2;
+  Method = 3;
 
   % list of simulation cases
   CaseList = {
@@ -55,6 +63,11 @@ function [ ] = GenWindNoVortex()
     SxlocVname = '/press_cent_xloc';
     SylocVname = '/press_cent_yloc';
 
+    UoutVname = '/u_orig';
+    VoutVname = '/v_orig';
+    UnvOutVname = '/u_nv';
+    VnvOutVname = '/v_nv';
+
     fprintf('  Reading: %s (%s)\n', UinFile, Uvname);
     fprintf('  Reading: %s (%s)\n', VinFile, Vvname);
     fprintf('  Reading: %s (%s)\n', FinFile, Fvname);
@@ -83,34 +96,41 @@ function [ ] = GenWindNoVortex()
       StormLocY = squeeze(h5read(ScFile, SylocVname));
     end
  
-    % Create the output files and dataset so that output can be written one time step at a time.
-    UoutFile = sprintf('HDF5/%s/HDF5/no_vortex_u_lite-%s-AS-2006-08-20-120000-g3.h5', Case, Case);
-    VoutFile = sprintf('HDF5/%s/HDF5/no_vortex_v_lite-%s-AS-2006-08-20-120000-g3.h5', Case, Case);
-
-    if (exist(UoutFile, 'file') == 2)
-      delete(UoutFile);
+    % Create the output file and datasets so that output can be written one time step at a time.
+    OutFile = sprintf('HDF5/%s/HDF5/no_vortex_lite-%s-AS-2006-08-20-120000-g3.h5', Case, Case);
+    if (exist(OutFile, 'file') == 2)
+      delete(OutFile);
     end
-    if (exist(VoutFile, 'file') == 2)
-      delete(VoutFile);
-    end
-
-    fprintf('  Writing %s (%s)\n', UoutFile, Uvname);
-    fprintf('  Writing %s (%s)\n', VoutFile, Vvname);
-    fprintf('\n');
 
     % copy the dimension vars into the output file
-    CreateDimensionsXyzt(UoutFile, X, Y, Z, T, Xvname, Yvname, Zvname, Tvname);
-    CreateDimensionsXyzt(VoutFile, X, Y, Z, T, Xvname, Yvname, Zvname, Tvname);
-
-    NotateDimensionsXyzt(UoutFile, Xvname, Yvname, Zvname, Tvname);
-    NotateDimensionsXyzt(VoutFile, Xvname, Yvname, Zvname, Tvname);
+    CreateDimensionsXyzt(OutFile, X, Y, Z, T, Xvname, Yvname, Zvname, Tvname);
+    NotateDimensionsXyzt(OutFile, Xvname, Yvname, Zvname, Tvname);
 
     % create output datasets that can be written one time step at a time
     Vsize = [ Nx Ny Nz Inf ];
     Csize = [ Nx Ny Nz   1 ];
-    h5create(UoutFile, Uvname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
-    h5create(VoutFile, Vvname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
 
+    h5create(OutFile, UoutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+    h5create(OutFile, VoutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+    h5create(OutFile, UnvOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+    h5create(OutFile, VnvOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+
+    switch(Method)
+      case 3
+        UbOutVname = '/u_basic';      
+        VbOutVname = '/v_basic';      
+  
+        h5create(OutFile, UbOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+        h5create(OutFile, VbOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+
+        UdOutVname = '/u_dist';      
+        VdOutVname = '/v_dist';      
+  
+        h5create(OutFile, UdOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+        h5create(OutFile, VdOutVname, Vsize, 'DataType', 'single', 'ChunkSize', Csize, 'Deflate', 6, 'Shuffle', true);
+    end
+
+    fprintf('  Extracting vortex\n');
     for it = 1:2 %Nt
       Start     = [ 1 1 1 it ];
       IoCount   = [ Nx Ny Nz 1 ];
@@ -122,7 +142,14 @@ function [ ] = GenWindNoVortex()
       FILTER = squeeze(h5read(FinFile, Fvname, Start, FiltCount));
       FILTER_3D = repmat(FILTER, [ 1 1 Nz ]);
 
+      h5write(OutFile, UoutVname, U, Start, IoCount);
+      h5write(OutFile, VoutVname, V, Start, IoCount);
+
+      U_NV = single(zeros([ Nx Ny Nz ]));
+      V_NV = single(zeros([ Nx Ny Nz ]));
+
       % Each method reads from U and V and places results in U_NV and V_NV.
+      % Each method can write intermediate results as needed.
       switch(Method)
         case 1
           % Replace entries in U and V with nans wherever filter is a '1'
@@ -130,14 +157,16 @@ function [ ] = GenWindNoVortex()
           V(FILTER_3D == 1) = nan;
 
           % Interpolate level by level
-          U_NV = zeros([ Nx Ny Nz ]);
-          V_NV = zeros([ Nx Ny Nz ]);
           for k = 1:Nz
             U_NV(:,:,k) = single(inpaintn(squeeze(U(:,:,k))));
             V_NV(:,:,k) = single(inpaintn(squeeze(V(:,:,k))));
           end
 
         case 2
+          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          % WARNING - WORK IN PROGRESS
+          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          
           % Subtract the storm motion from U and V
           %U = U - StormSpeedX(it);
           %V = V - StormSpeedY(it);
@@ -180,10 +209,6 @@ function [ ] = GenWindNoVortex()
           % each x,y location belongs to.
           R_SELECT = int32(RADIUS ./ Rinc) + 1;
 
-figure;
-contourf(R_SELECT',[ 1:20 ]);
-colorbar;
-
           AzavgVr = zeros([ Nb Nz ]);
           AzavgVt = zeros([ Nb Nz ]);
 
@@ -195,7 +220,7 @@ colorbar;
 %              end
 %            end
 %          end
-
+%
 %          for ir = 1:Nb-1
 %            Select = (RADIUS >= Rbins(ir)) & (RADIUS < Rbins(ir+1));
 %            for iz = 1:Nz
@@ -206,13 +231,18 @@ colorbar;
 %            end
 %          end
 
-figure;
-contourf(AzavgVt',20);
-colorbar;
+        case 3
+          % Create the basic field by smoothing the input fields.
+          [ U_B ] = KbrSmoothing(U,X,Y,Z);
+          [ V_B ] = KbrSmoothing(V,X,Y,Z);
+          h5write(OutFile, UbOutVname, U_B, Start, IoCount);
+          h5write(OutFile, VbOutVname, V_B, Start, IoCount);
 
-%DEBUG
-U_NV = VR;
-V_NV = VT;
+          % Disturbance field is original field minus the basic field
+          U_D = U - U_B;
+          V_D = V - V_B;
+          h5write(OutFile, UdOutVname, U_D, Start, IoCount);
+          h5write(OutFile, VdOutVname, V_D, Start, IoCount);
 
         otherwise
           if (it == 1)
@@ -224,20 +254,93 @@ V_NV = VT;
           V_NV = V;
       end
 
-      h5write(UoutFile, Uvname, U_NV, Start, IoCount);
-      h5write(VoutFile, Vvname, V_NV, Start, IoCount);
+      h5write(OutFile, UnvOutVname, U_NV, Start, IoCount);
+      h5write(OutFile, VnvOutVname, V_NV, Start, IoCount);
 
       if (mod(it, 10) == 0)
-        fprintf('  Completed timestep: %d\n', it);
+        fprintf('    Completed timestep: %d\n', it);
       end
     end
-    fprintf('  Processed %d timesteps\n', it);
+    fprintf('    Processed %d timesteps\n', it);
     fprintf('\n');
 
     % attach dimensions to output u,v for GRADS
+    fprintf('  Writing %s\n', OutFile);
+
     DimOrder = { 'x' 'y' 'z' 't' };
-    AttachDimensionsXyzt(UoutFile, Uvname, DimOrder, Xvname, Yvname, Zvname, Tvname);
-    AttachDimensionsXyzt(VoutFile, Vvname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+    fprintf('    %s\n', UoutVname);
+    fprintf('    %s\n', VoutVname);
+    fprintf('    %s\n', UnvOutVname);
+    fprintf('    %s\n', VnvOutVname);
+    AttachDimensionsXyzt(OutFile, UoutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+    AttachDimensionsXyzt(OutFile, VoutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+    AttachDimensionsXyzt(OutFile, UnvOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+    AttachDimensionsXyzt(OutFile, VnvOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+
+    switch(Method)
+      case 3
+        fprintf('    %s\n', UbOutVname);
+        fprintf('    %s\n', VbOutVname);
+        AttachDimensionsXyzt(OutFile, UbOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+        AttachDimensionsXyzt(OutFile, VbOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+
+        fprintf('    %s\n', UdOutVname);
+        fprintf('    %s\n', VdOutVname);
+        AttachDimensionsXyzt(OutFile, UdOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+        AttachDimensionsXyzt(OutFile, VdOutVname, DimOrder, Xvname, Yvname, Zvname, Tvname);
+    end
 
   end % cases
 end % function
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% KbrSmoothing()
+%
+% This function performs the smoothing described in Kurihara et al. (1993)
+% on a single field.
+%
+function [ VarSmooth ] = KbrSmoothing(Var,X,Y,Z)
+  % K coefficient values from Kurihara et al. (1993)
+  M = [ 2 3 4 2 5 6 7 2 8 9 2 ];
+  K = 0.5 ./ (1-cos((2*pi)./M));
+  Nk = length(K);
+
+  % Kurihara et al. (1993) specified the formula for 1 degree grid spacing, and for
+  % filtering out components up to 9 degrees wavelength. This data is on a 1/3 degree
+  % grid spacing. When I tried to adjust the formula so that it would still filter
+  % up to 9 degree wavelength, the function blew up. To deal with this, sample the
+  % input Var every 3rd point to get gridded data on a 1 degree grid.
+  Vinc = 3;
+  VarSmoothLow = Var(1:Vinc:end,1:Vinc:end,:);
+
+  % First smooth in the x direction
+  for i = 1:Nk
+    VarIm1 = VarSmoothLow(1:end-2,:,:);
+    VarI   = VarSmoothLow(2:end-1,:,:);
+    VarIp1 = VarSmoothLow(3:end,  :,:);
+
+    VarSmoothLow(2:end-1,:,:) = VarSmoothLow(2:end-1,:,:) + K(i) .* (VarIm1 + VarIp1 - 2.*VarI);
+  end
+  clear VarIm1;
+  clear VarI;
+  clear VarIp1;
+
+  % Second smooth in the y direction
+  for i = 1:Nk
+    VarJm1 = VarSmoothLow(:,1:end-2,:);
+    VarJ   = VarSmoothLow(:,2:end-1,:);
+    VarJp1 = VarSmoothLow(:,3:end,  :);
+
+    VarSmoothLow(:,2:end-1,:) = VarSmoothLow(:,2:end-1,:) + K(i) .* (VarJm1 + VarJp1 - 2.*VarJ);
+  end
+  clear VarJm1;
+  clear VarJ;
+  clear VarJp1;
+
+  % VarSmoothLow is at 1/3 resolution than Var. Interpolate back to the higher
+  % resolution so that it matches Var. [XYZ]L are gridded coordinate values
+  % for low resolution grid, [XYZ]H are same for high resolution grid.
+  [ XL, YL, ZL ] = ndgrid(X(1:Vinc:end),Y(1:Vinc:end),Z);
+  [ XH, YH, ZH ] = ndgrid(X,Y,Z);
+  VarSmooth = interpn(XL,YL,ZL,VarSmoothLow,XH,YH,ZH);
+end
