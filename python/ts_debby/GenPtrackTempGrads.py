ad84237a7b545842f73854da40a4740cb8cd4789
steve <sherbener@atmos.colostate.edu>
2017-03-09 16:12:29 -0700

    Added horzontal gradient of the 2D field (z,x). Added smoothing before gradient calculation in order to mitigate noise.

--- python/ts_debby/GenPtrackTempGrads.py
+++ python/ts_debby/GenPtrackTempGrads.py
@@ -8,10 +8,36 @@ sys.path.append("{0:s}/etc/python/ts_debby".format(os.environ['HOME']))
 import h5py
 import numpy as np
 import scipy.integrate as integ
-import scipy.ndimage.filters as filt
+import pandas as pd
 import ConfigTsd as conf
 import Hdf5Utils as h5u
 
+#######################################################################################
+# Functions
+#######################################################################################
+
+#####################################
+# SmoothLine
+#
+# Remove noise from a linear data series.
+#
+def SmoothLine(Var):
+    # Use the ewma (exponentially weighted moving average) from the
+    # scipy package to smooth a data series. Apply both forward and
+    # backward in order to eliminate phase delay.
+
+    FiltCom = 10 # Center of mass spec for ewma function. alpha = 1 / ( 1 + FiltCom)
+
+    Forward  = pd.ewma(Var, com=FiltCom)
+    Backward = pd.ewma(Var[::-1], com=FiltCom)
+    VarSmooth = np.vstack((Forward, Backward[::-1]))
+    VarSmooth = np.squeeze(np.mean(VarSmooth, axis=0))
+
+    return VarSmooth
+
+#######################################################################################
+# Main
+#######################################################################################
 
 Tstring = conf.SetTimeString()
 RhoAir = conf.SetRhoAir()
@@ -59,7 +85,7 @@ for isim in range(Nsims):
     NoZ = True
     NoP = True
     NoT = True
-    
+
     OutFname = "DIAGS/ptrack_tgrads_{0:s}.h5".format(Sim)
     Ofile = h5py.File(OutFname, mode='w')
 
@@ -80,6 +106,13 @@ for isim in range(Nsims):
             Nx = len(X)
             Xdim = h5u.DimCoards(Xname, 1, [ Nx ], 'x')
             Xdim.Build(Ofile, X)
+
+            # Create delta X: dx[i] = x[i+1] - x[i]
+            # then repeat the last dx[i] to make delta X length
+            # match that of X
+            DeltaX = np.diff(X)
+            DeltaX = np.append(DeltaX, DeltaX[-1])
+
             NoX = False
 
         if (NoY):
@@ -129,43 +162,90 @@ for isim in range(Nsims):
 
         Ifile.close()
 
-        # Do vertical average of layer between bottom and top layer
-        # Use Simpson's Rule integration to the do the averaging
+        # Record items that depend on VcoordType
         if (VcoordType == 'z'):
+            Nvert = Nz
+
             Height = Z[Z2] - Z[Z1]
             Hselect = Z[Z1:Z2]
-            Hselect = Hselect.reshape((len(Hselect),1))
-            Hselect = np.tile(Hselect, (1, Nx))
+
             Tselect = np.squeeze(InVar[Z1:Z2,:])
+
+            VertDim = Zdim
         elif (VcoordType == 'p'):
+            Nvert = Np
+
             Height = P[P2] - P[P1]
             Hselect = P[P1:P2]
-            Hselect = Hselect.reshape((len(Hselect),1))
-            Hselect = np.tile(Hselect, (1, Nx))
+
             Tselect = np.squeeze(InVar[P1:P2,:])
+
+            VertDim = Pdim
+
+        # Smooth the input temperature field in the horizontal.
+        InVarSmooth = np.zeros((Nvert,Nx))
+        for ivert in range(Nvert):
+            InVarSmooth[ivert,:] = SmoothLine(np.squeeze(InVar[ivert,:]))
+
+        # Take the horizontal gradient of the 2D field.
+        #   Create a 2D delta X array
+        DeltaX_2D = np.tile(DeltaX.reshape((1,Nx)), (Nvert,1))
+        InVarGrad = np.gradient(InVarSmooth, DeltaX_2D, axis=1)
+        InVarGradSmooth = np.zeros((Nvert,Nx))
+        for ivert in range(Nvert):
+            InVarGradSmooth[ivert,:] = SmoothLine(np.squeeze(InVarGrad[ivert,:]))
+
+        # Do vertical average of layer between bottom and top layer
+        # Use Simpson's Rule integration to the do the averaging
+        Hselect = Hselect.reshape((len(Hselect),1))
+        Hselect = np.tile(Hselect, (1, Nx))
         Tbar = np.squeeze(integ.simps(Tselect, Hselect, axis=0)) / Height
 
         # Smooth Tbar in order to mitigate noise in the gradient
-        Tbar = filt.convolve(Tbar, np.ones(FiltLen)/float(FiltLen), mode='reflect')
+        Tbar = SmoothLine(Tbar)
 
-        # Temperature gradient
-        DeltaX = np.diff(X)
-        DeltaX = np.append(DeltaX, DeltaX[-1])
+        # Gradient of Tbar (mean temperature of layer)
         Tgrad = np.gradient(Tbar, DeltaX)
-        TgradSmooth = filt.convolve(Tgrad, np.ones(FiltLen)/float(FiltLen), mode='reflect')
+        TgradSmooth = SmoothLine(Tgrad)
          
-        # Write out Tbar, gradient, plus smoothed version of gradient
+        # Output:
+        #  original field
+        #  smoothed field
+        #  gradient of field
+        #  smoothed gradient of field
+        #  Tbar
+        #  gradient based on Tbar
+        #  smoothed gradient based on Tbar
+        print("  Writing {0:s} ({1:s})".format(OutFname, InVname))
+        TempDset = h5u.DsetCoards(InVname, 2, [ Nvert, Nx ])
+        TempDset.Build(Ofile, InVar, VertDim, Xdim)
+
+        OutVname = "{0:s}_smooth".format(InVname)
+        print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
+        TempDset = h5u.DsetCoards(OutVname, 2, [ Nvert, Nx ])
+        TempDset.Build(Ofile, InVarSmooth, VertDim, Xdim)
+
+        OutVname = "{0:s}_hgrad".format(InVname)
+        print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
+        TempDset = h5u.DsetCoards(OutVname, 2, [ Nvert, Nx ])
+        TempDset.Build(Ofile, InVarGrad, VertDim, Xdim)
+
+        OutVname = "{0:s}_hgrad_smooth".format(InVname)
+        print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
+        TempDset = h5u.DsetCoards(OutVname, 2, [ Nvert, Nx ])
+        TempDset.Build(Ofile, InVarGradSmooth, VertDim, Xdim)
+
         OutVname = "{0:s}_bar".format(InVname)
         print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
         TbarDset = h5u.DsetCoards(OutVname, 1, [ Nx ])
         TbarDset.Build(Ofile, Tbar, Xdim)
 
-        OutVname = "{0:s}_hgrad".format(InVname)
+        OutVname = "{0:s}_bar_hgrad".format(InVname)
         print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
         TgradDset = h5u.DsetCoards(OutVname, 1, [ Nx ])
         TgradDset.Build(Ofile, Tgrad, Xdim)
 
-        OutVname = "{0:s}_hgrad_smooth".format(InVname)
+        OutVname = "{0:s}_bar_hgrad_smooth".format(InVname)
         print("  Writing {0:s} ({1:s})".format(OutFname, OutVname))
         TgradSmoothDset = h5u.DsetCoards(OutVname, 1, [ Nx ])
         TgradSmoothDset.Build(Ofile, TgradSmooth, Xdim)
