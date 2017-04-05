627b59e3326b685bd84ce63ac6cfc6fecc62e524
steve <sherbener@atmos.colostate.edu>
2017-04-05 12:32:01 -0600

    Started python scripts for RCE simulations

---
+++ python/nasa_wisc/ConfigRce.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def SetTimeString():
+    # Set TimeString to the beginning of the TS Debby spin up simulation
+    TimeString = '2012-01-01 00:00:00 00:00'
+    return TimeString
+
+def SetColorScheme():
+    ColorScheme = {
+        'RCE_S300_SQ'     : 'black',
+        }
+    return ColorScheme
+
+def SetLabelScheme():
+    LabelScheme = {
+        'RCE_S300_SQ'     : 'S300-SQ',
+        }
+    return LabelScheme
