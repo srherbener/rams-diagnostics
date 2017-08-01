Fixes for ventilation index:   Using wind fields that have been smoothed (vortex removal) across entire field.   Using a radius of 2000 km for averaging: shear, To, and Ts   Using SAL as the To level (for potential intensity) since wind shear is also using that.     (using To at real outflow level doesn't capture detrimental effect of mid-level      heating by the SAL)

python/ts_debby/GenVentIndex.py
@@ -137,12 +137,15 @@ Zbl1 = 0
 Zbl2 = 2
 
 # For outflow layer: 300 - 200 mb
-Zout1 = 6
-Zout2 = 8
+#Zout1 = 6 # Bister and Emanuel (2002)
+#Zout2 = 8
+Zout1 = 3 # For SAL case, wind shear is calculated using
+Zout2 = 3 # surface and mid-level (SAL location) layers. Do same for PI.
 
 # Radius values for averaging regions
 Rin  = 150.0 # km
 Rout = 300.0 # km
+Renv = 2000.0 # km
 
 # Ck/Cd (ratio of enthalpy exchange coefficient to drag coefficient) value is
 # from Bell et al. (2012) and Haus et al. (2010). TS Debby was a weak storm
@@ -267,7 +270,7 @@ for isim in range(Nsims):
         Tout = LayerAverage(Tnv, DensNv, Zout1, Zout2)
 
         ######## Wind Shear ##############
-        WindShear[it] = np.mean(MagShear[Radius <= Rout])
+        WindShear[it] = np.mean(MagShear[Radius <= Renv])
 
         ########## Entropy Deficit ############
         Sm[it]    = np.mean(Smidlevel[(Radius >= Rin) * (Radius <= Rout)])
@@ -278,8 +281,8 @@ for isim in range(Nsims):
 
 
         ########## Potential Intensity ############
-        Ts[it] = np.mean(TsstNv[Radius <= Rout])
-        To[it] = np.mean(Tout[Radius <= Rout])
+        Ts[it] = np.mean(TsstNv[Radius <= Renv])
+        To[it] = np.mean(Tout[Radius <= Renv])
 
 
         if ((it % 10) == 0):
@@ -307,16 +310,16 @@ for isim in range(Nsims):
     SbSmooth      = nu.SmoothLine(Sb)
     SsstSatSmooth = nu.SmoothLine(SsstSat)
 
-    SdefSmooth = (SmSatSmooth - SmSmooth) / (SsstSatSmooth - Sb)
+    SdefSmooth = nu.SmoothLine((SmSatSmooth - SmSmooth) / (SsstSatSmooth - Sb))
 
     # potential intensity
     TsSmooth = nu.SmoothLine(Ts)
     ToSmooth = nu.SmoothLine(To)
 
-    PiSmooth = np.sqrt((TsSmooth - ToSmooth) * (TsSmooth / ToSmooth) * CkOverCd * (SsstSatSmooth - SbSmooth))
+    PiSmooth = nu.SmoothLine(np.sqrt((TsSmooth - ToSmooth) * (TsSmooth / ToSmooth) * CkOverCd * (SsstSatSmooth - SbSmooth)))
 
     # ventilation index
-    ViSmooth = WindShearSmooth * SdefSmooth / PiSmooth
+    ViSmooth = nu.SmoothLine(WindShearSmooth * SdefSmooth / PiSmooth)
 
 
     # Create output with COARDS convention

python/ts_debby/GenWindShearField.py
@@ -30,13 +30,17 @@ FilterVname = "/filter"
 if (Vcoord == 'p'):
   UfnameTemplate = "HDF5/<SIM>/HDF5/u_nv_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
   VfnameTemplate = "HDF5/<SIM>/HDF5/v_nv_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
+  DfnameTemplate = "HDF5/<SIM>/HDF5/density_nv_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
   #UfnameTemplate = "HDF5/<SIM>/HDF5/u_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
   #VfnameTemplate = "HDF5/<SIM>/HDF5/v_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
+  #DfnameTemplate = "HDF5/<SIM>/HDF5/density_lite-<SIM>-AP-2006-08-20-120000-g3.h5"
 elif (Vcoord == 'z'):
   UfnameTemplate = "HDF5/<SIM>/HDF5/u_nv_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
   VfnameTemplate = "HDF5/<SIM>/HDF5/v_nv_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
+  DfnameTemplate = "HDF5/<SIM>/HDF5/density_nv_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
   #UfnameTemplate = "HDF5/<SIM>/HDF5/u_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
   #VfnameTemplate = "HDF5/<SIM>/HDF5/v_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
+  #DfnameTemplate = "HDF5/<SIM>/HDF5/density_lite-<SIM>-AS-2006-08-20-120000-g3.h5"
 else:
   print("ERROR: must set Vcoord to either 'z' or 'p'")
   exit(1)
@@ -47,6 +51,7 @@ Vvname = "/v_basic"
 #Vvname = "/v_orig"
 #Uvname = "/u"
 #Vvname = "/v"
+Dvname = "/density"
 
 OfnameTemplate = "DIAGS/wind_shear_nv_lite_<SIM>.h5"
 UshearVname = "/u_shear"
@@ -86,19 +91,28 @@ Tname = '/t_coords'
 #  SFC top        925 mb          761 m
 
 if (Vcoord == 'p'):
+    ## Bulk environmental wind shear
+    ## pressure coords
+    #ZsalBot = 250
+    #ZsalTop = 150
+    #
+    #ZsfcBot =  925
+    #ZsfcTop =  700
+
+    # Surface and SAL
     # pressure coords
     ZsalBot = 700
     ZsalTop = 700
+    
+    ZsfcBot = 925
+    ZsfcTop = 925
 
-    ZsfcBot =  925
-    ZsfcTop =  925
-
-# Dunion and Velden, 2004
-#    ZsalBot = 300
-#    ZsalTop = 150
-#
-#    ZsfcBot =  925
-#    ZsfcTop =  700
+    ## Dunion and Velden, 2004
+    #ZsalBot = 300
+    #ZsalTop = 150
+    #
+    #ZsfcBot =  925
+    #ZsfcTop =  700
 
     Vunits = "mb"
 
@@ -125,11 +139,13 @@ for isim in range(Nsims):
     FilterFname = FilterFnameTemplate.replace("<SIM>", Sim)
     Ufname      = UfnameTemplate.replace("<SIM>", Sim)
     Vfname      = VfnameTemplate.replace("<SIM>", Sim)
+    Dfname      = DfnameTemplate.replace("<SIM>", Sim)
     Ofname      = OfnameTemplate.replace("<SIM>", Sim)
 
     print("  Reading {0:s} ({1:s})".format(FilterFname, FilterVname))
     print("  Reading {0:s} ({1:s})".format(Ufname, Uvname))
     print("  Reading {0:s} ({1:s})".format(Vfname, Vvname))
+    print("  Reading {0:s} ({1:s})".format(Dfname, Dvname))
     print("")
 
     # Read input data. If this is the first set, read in the coordinates
@@ -137,6 +153,7 @@ for isim in range(Nsims):
     Ffile = h5py.File(FilterFname, mode='r')
     Ufile = h5py.File(Ufname, mode='r')
     Vfile = h5py.File(Vfname, mode='r')
+    Dfile = h5py.File(Dfname, mode='r')
 
     Ofile = h5py.File(Ofname, mode='w')
 
@@ -219,18 +236,19 @@ for isim in range(Nsims):
         FILTER = np.squeeze(Ffile[FilterVname][it,...])
         U = np.squeeze(Ufile[Uvname][it,...])
         V = np.squeeze(Vfile[Vvname][it,...])
+        D = np.squeeze(Dfile[Dvname][it,...])
 
         # Python indexing Z1:Z2 stops one before Z2. We want Z2 included so  use
         # indexing Z1:Z2+1.
         #
-        # Take mean on z-dimension (1st dimension) which will yield a layer average.
+        # Take density weighted average along z-dimension (1st dimension).
         #
         # Then take the difference between layers to get the u,v components of the shear vectors.
-        U_SFC = np.squeeze(np.mean(U[SFC_Z1:SFC_Z2+1,...],0))
-        U_SAL = np.squeeze(np.mean(U[SAL_Z1:SAL_Z2+1,...],0))
+        U_SFC = np.squeeze(np.average(U[SFC_Z1:SFC_Z2+1,...], axis=0, weights=D[SFC_Z1:SFC_Z2+1,...]))
+        U_SAL = np.squeeze(np.average(U[SAL_Z1:SAL_Z2+1,...], axis=0, weights=D[SAL_Z1:SAL_Z2+1,...]))
 
-        V_SFC = np.squeeze(np.mean(V[SFC_Z1:SFC_Z2+1,...],0))
-        V_SAL = np.squeeze(np.mean(V[SAL_Z1:SAL_Z2+1,...],0))
+        V_SFC = np.squeeze(np.average(V[SFC_Z1:SFC_Z2+1,...], axis=0, weights=D[SFC_Z1:SFC_Z2+1,...]))
+        V_SAL = np.squeeze(np.average(V[SAL_Z1:SAL_Z2+1,...], axis=0, weights=D[SAL_Z1:SAL_Z2+1,...]))
 
         # Cartesian
         U_SHEAR = U_SAL - U_SFC