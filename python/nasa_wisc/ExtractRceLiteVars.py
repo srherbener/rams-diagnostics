d8f37152dda7d4795eaf03af8283d7464e6cbb80
steve <sherbener@atmos.colostate.edu>
2017-08-18 10:43:08 -0600

    Got simple extraction of RCE lite vars working!

--- python/nasa_wisc/ExtractRceLiteVars.py
+++ python/nasa_wisc/ExtractRceLiteVars.py
@@ -22,8 +22,8 @@ Nx = len(X)
 Y = conf.SetHcoords(-2.15497, 2.15497, 480)
 Ny = len(Y)
 
-Z = conf.SetZcoords()
-Nz = len(Z)
+Zall = conf.SetZcoords()
+NzAll = len(Zall)
 
 Xname = "/x_coords"
 Yname = "/y_coords"
@@ -39,9 +39,16 @@ SimList = [
     ]
 Nsims = len(SimList)
 
+VarList = [
+    'total_cond',
+    'precip_rate',
+    'vapor',
+    ]
+Nvars = len(VarList)
+
 #InFilePatternTemplate = "RAMS/<SIM>/RAMS/<SIM>-L-2012-*.h5"
-InFilePatternTemplate = "SIMDATA/<SIM>/RAMS/<SIM>-L-2012-01-01-0*.h5"
-OutFileTemplate = "SIMDATA/<SIM>/HDF5/rce_vars-<SIM>-AC-2012-01-01-000000-g1.h5"
+InFilePatternTemplate = "SIMDATA/<SIM>/RAMS/<SIM>-L-2012-01-01-0[0-4]*.h5"
+OutFileTemplate = "SIMDATA/<SIM>/HDF5/<VNAME>-<SIM>-AC-2012-01-01-000000-g1.h5"
 
 for isim in range(Nsims):
     Sim = SimList[isim]
@@ -55,65 +62,79 @@ for isim in range(Nsims):
     Nt = len(FileList)
     T = np.arange(Nt) * 3600.0 # sim time in seconds, start at zero
 
-    # Open the output file
-    OutFname = OutFileTemplate.replace("<SIM>", Sim)
-    print("OutFname", OutFname)
-    OutFile = h5py.File(OutFname, mode='w')
+    # Open all of the output files
+    OutFnames = []
+    OutFiles = []
+    for ivar in range(Nvars):
+        Vname = VarList[ivar]
 
-    # Build the coordinates in the output file
-    Xdim = h5u.DimCoards(Xname, 1, [ Nx ], 'x')
-    Xdim.Build(OutFile, X)
+        OutFnames.append(OutFileTemplate.replace("<SIM>", Sim).replace("<VNAME>", Vname))
+        OutFiles.append(h5py.File(OutFnames[ivar], mode='w'))
 
-    Ydim = h5u.DimCoards(Yname, 1, [ Ny ], 'y')
-    Ydim.Build(OutFile, Y)
+    # Process one input file at a time, read in all vars and write into
+    # output files.
+    it = 0
+    OutDsets = []
+    for InFname in sorted(FileList):
+        print("  Reading: {0:s}, Time step: {1:d}".format(InFname, it+1))
+        InFile = h5py.File(InFname, mode='r')
 
-    Zdim = h5u.DimCoards(Zname, 1, [ Nz ], 'z')
-    Zdim.Build(OutFile, Z)
+        for ivar in range(Nvars):
+            Vname = VarList[ivar]
+            print("    Variable: {0:s}".format(Vname))
 
-    Tdim = h5u.DimCoards(Tname, 1, [ Nt ], 't', tstring=Tstring)
-    Tdim.Build(OutFile, T)
+            if (Vname == 'total_cond'):
+                RTP = InFile['RTP'][...]
+                RV  = InFile['RV'][...]
+                VAR = RTP - RV
+            elif (Vname == 'precip_rate'):
+                VAR = InFile['PCPRR'][...]
+            elif (Vname == 'vapor'):
+                VAR = InFile['RV'][...]
+            else:
+                print("      Warning: undefined variable ({0:s}), skipping this variable".format(Vname))
+                continue
 
-    
+            # Cast a 2D variable into a 3D variable with a z-dimension length of 1
+            if (len(VAR.shape) == 2):
+                Z = [ 0 ]
+                Nz = 1
+                VAR = VAR.reshape((1, VAR.shape[0], VAR.shape[1]))
+            else:
+                Z = Zall
+                Nz = NzAll
 
+            # If this is the first time step, build the coordinates and dataset for the variable
+            if (it == 0):
+                # Build the coordinates in the output file
+                Xdim = h5u.DimCoards(Xname, 1, [ Nx ], 'x')
+                Xdim.Build(OutFiles[ivar], X)
+
+                Ydim = h5u.DimCoards(Yname, 1, [ Ny ], 'y')
+                Ydim.Build(OutFiles[ivar], Y)
+
+                Zdim = h5u.DimCoards(Zname, 1, [ Nz ], 'z')
+                Zdim.Build(OutFiles[ivar], Z)
+
+                Tdim = h5u.DimCoards(Tname, 1, [ Nt ], 't', tstring=Tstring)
+                Tdim.Build(OutFiles[ivar], T)
+
+                OutDsets.append(h5u.DsetCoards(Vname, 4, (Nt, Nz, Ny, Nx), chunks=(1, Nz, Ny, Nx)))
+                OutDsets[ivar].Create(OutFiles[ivar])
+
+            # Write the var into the output file
+            OutFiles[ivar][Vname][it,...] = VAR
+
+        InFile.close()
+        it = it + 1
 
     print("")
-    OutFile.close()
 
+    # Attach dims and close the output files
+    for ivar in range(Nvars):
+        print("  Writing: {0:s} ({1:s})".format(OutFnames[ivar], VarList[ivar]))
+        OutDsets[ivar].AttachDims(OutFiles[ivar], Tdim, Zdim, Ydim, Xdim)
+        OutFiles[ivar].close()
 
+    print("")
 
-### 
-###     # Read in input variable and form the domain average
-###     # Do one time step at a time in order to reduce memory usage
-###     for i in range(Nt):
-###         # Read in the var and determine if 2D (t,y,x) or 3D (t,z,y,x) field.
-###         Var = np.squeeze(InFile[InVname][i,...])
-###         Ndims = Var.ndim
-### 
-###         if (Ndims == 2):
-###             # Var is (y,x)
-###             if (i == 0):
-###                 VarAvg = np.zeros(Nt)
-###                 VarAvg[i] = CalcHorizAvg(Var, Select)
-###         elif (Ndims == 3):
-###             # Var is (z,y,x)
-###             if (i == 0):
-###                 VarAvg = np.zeros((Nt,Nz))
-###             for k in range(Nz):
-###                 VarAvg[i,k] = CalcHorizAvg(np.squeeze(Var[i,:,:]), Select)
-### 
-###         if ((i % 10) == 0):
-###             print("    Processing time step: {0:d}".format(i))
-### 
-###     print("")
-### 
-###     InFile.close()
-### 
-###     # Write out average
-###     print("  Writing: {0:s} ({1:s})".format(OutFname, OutVname))
-###     Dset = h5u.DsetCoards(OutVname, Ndims, VarAvg.shape)
-###     if (Ndims == 2):
-###         Dset.Build(OutFile, VarAvg, Tdim)
-###     elif (Ndims == 3):
-###         Dset.Build(OutFile, VarAvg, Tdim, Zdim)
-### 
-###     print("")
