9b0619ee6d8434b6cfb2768517211f48ec68e3bb
steve <sherbener@atmos.colostate.edu>
2017-09-20 13:29:44 -0600

    Added plotting of power spectra.

---
+++ python/nasa_wisc/GenPowerSpectra.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+sys.path.append("{0:s}/etc/python/common".format(os.environ['HOME']))
+sys.path.append("{0:s}/etc/python/nasa_wisc".format(os.environ['HOME']))
+
+import h5py
+import numpy as np
+import ConfigRce as conf
+import Hdf5Utils as h5u
+from scipy import signal as sig
+
+
+Tstring = conf.SetTimeString()
+
+SimList = [
+#    'RCE_1km',
+#    'RCE_1km_SM',
+#    'RCE_1km_DM',
+#    'RCE_1km_DP',
+#    'RCE_1km_IPL2',
+#    'RCE_1km_SM_IPL2',
+#    'RCE_1km_DM_IPL2',
+#    'RCE_1km_DP_IPL2',
+    'RCE_3km_1mom',
+    'RCE_3km_1mom_db',
+    'RCE_3km_1mom_dm',
+#    'RCE_3km_2mom',
+    'RCE_3km_2mom_db',
+    'RCE_3km_2mom_dm',
+    'RCE_3km_2mom_dm_lrz',
+    ]
+Nsims = len(SimList)
+
+VarList = [
+    [ "dom_avg_pcprr",      "/pcprr"      ],
+    [ "dom_avg_vint_vapor", "/vint_vapor" ],
+    [ "dom_avg_top_lwup",   "/top_lwup"   ],
+    ] 
+Nvars = len(VarList)
+
+# MAIN
+for isim in range(Nsims):
+    Sim = SimList[isim]
+    print("************************************************************************************")
+    print("Generating power spectra for simulation: {0:s}".format(Sim))
+    print("")
+
+    # Open the output file
+    OutFname = "DIAGS/power_spectra_{0:s}.h5".format(Sim)
+    OutFile = h5py.File(OutFname, mode='w')
+
+    # Read input variables, calculate horizontal averages and write out results
+    for ivar in range(Nvars):
+        Fprefix = VarList[ivar][0]
+        Vname   = VarList[ivar][1]
+
+        # Open the input file
+
+        InFname = "DIAGS/{0:s}_{1:s}.h5".format(Fprefix, Sim)
+        InFile  = h5py.File(InFname, mode='r')
+
+        # Read in input variable and form the temporal average using the
+        # Tmin and Tmax values to select a range of time steps. Vars will
+        # be of the form (t,z).
+        print("  Reading: {0:s} ({1:s})".format(InFname, Vname))
+        if (Vname == "/pcprr"):
+            VAR = InFile[Vname][...] * 24.0 # convert mm/h to mm/day
+        else:
+            VAR = InFile[Vname][...]
+        print("")
+
+        # Calculate the power spectrum. Assume that the points in VAR are evenly spaced.
+        VarFreq, VarPsd = sig.welch(VAR)
+        Nf = len(VarFreq)
+
+        # Write out average
+        OutVname = "{0:s}_psd".format(Vname)
+        print("  Writing: {0:s} ({1:s})".format(OutFname, OutVname))
+        OutDset = h5u.BaseDset(OutVname, 1, [ Nf ])
+        Dset = OutDset.Create(OutFile)
+        Dset[...] = VarPsd
+
+        OutVname = "{0:s}_freq".format(Vname)
+        print("  Writing: {0:s} ({1:s})".format(OutFname, OutVname))
+        OutDset = h5u.BaseDset(OutVname, 1, [ Nf ])
+        Dset = OutDset.Create(OutFile)
+        Dset[...] = VarFreq
+
+        print("")
+
+    InFile.close()
+    OutFile.close()
---
+++ python/nasa_wisc/PlotPowerSpectra.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+sys.path.append("{0:s}/etc/python/common".format(os.environ['HOME']))
+sys.path.append("{0:s}/etc/python/nasa_wisc".format(os.environ['HOME']))
+
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import PlotUtils as plu
+import ConfigRce as conf
+
+# color and label schemes for plotting
+ColorScheme = conf.SetColorScheme()
+LabelScheme = conf.SetLabelScheme()
+
+PlotList = [ 
+#    [ [ 'RCE_1km', 'RCE_1km_SM', 'RCE_1km_DM', 'RCE_1km_DP' ], r'$IPLAWS = 0$', 'RceIpl0', 0, 50 ],
+#    [ [ 'RCE_1km_IPL2', 'RCE_1km_SM_IPL2', 'RCE_1km_DM_IPL2', 'RCE_1km_DP_IPL2' ], r'$IPLAWS = 2$', 'RceIpl2', 0, 50 ],
+#    [ [ 'RCE_3km_1mom', 'RCE_3km_2mom', 'RCE_3km_2mom_db', 'RCE_3km_2mom_dm' ], '', 'Rce3km', 0, 80 ],
+
+    [ [ 'RCE_3km_1mom', 'RCE_3km_1mom_db', 'RCE_3km_1mom_dm' ], '', 'Rce3km1mom', [ 1.5, 300 ] ],
+    [ [ 'RCE_3km_2mom_db', 'RCE_3km_2mom_dm', 'RCE_3km_2mom_dm_lrz' ], '', 'Rce3km2mom', [ 1.5, 300 ] ],
+    ]
+Nplots = len(PlotList)
+
+VarList = [
+    [ 'DIAGS/power_spectra_<SIM>.h5',   '/pcprr',      [   1e-3,  1e3 ], r'$PSD(PR^2)$',  'Prate'  ],
+    [ 'DIAGS/power_spectra_<SIM>.h5',   '/vint_vapor', [   1e-5,  1e3 ], r'$PSD(PW^2)$',  'Pwater' ],
+    [ 'DIAGS/power_spectra_<SIM>.h5',   '/top_lwup',   [   1e-3,  1e4 ], r'$PSD(OLR^2)$', 'Olr'    ],
+    ]
+Nvars = len(VarList)
+
+# plot config
+Pdir = 'Plots.py'
+Xlabel = r'$Period (h)$'
+
+for iplot in range(Nplots):
+    SimList  = PlotList[iplot][0]
+    Pname    = PlotList[iplot][1]
+    Pfprefix = PlotList[iplot][2]
+    Xlims    = PlotList[iplot][3]
+
+    Nsims = len(SimList)
+
+    for ivar in range(Nvars):
+        InFileTemplate = VarList[ivar][0]
+        Vname          = VarList[ivar][1]
+        Ylims          = VarList[ivar][2]
+        Ylabel         = VarList[ivar][3]
+        Oname          = VarList[ivar][4]
+
+        print("Generating plot for varialbe: {0:s}".format(Vname))
+        PsdVname  = "{0:s}_psd".format(Vname)
+        FreqVname = "{0:s}_freq".format(Vname)
+
+        SimLegText = []
+        SimColors = []
+        for isim in range(Nsims):
+            Sim = SimList[isim]
+            SimLegText.append(LabelScheme[Sim])
+            SimColors.append(ColorScheme[Sim])
+         
+            InFname = InFileTemplate.replace("<SIM>", Sim)
+            InFile = h5py.File(InFname, mode='r')
+
+            print("  Reading: {0:s} ({1:s})".format(InFname, PsdVname))
+            PSD = InFile[PsdVname][...]
+            if (isim == 0):
+                print("  Reading: {0:s} ({1:s})".format(InFname, FreqVname))
+                FREQ = InFile[FreqVname][...]
+                PER = 1 / FREQ
+                Nx = len(PER)
+
+                VARS = np.zeros((Nsims,Nx))
+
+            VARS[isim,...] = PSD
+
+            InFile.close()
+
+        print("")
+
+        # Make the plot
+        Fig = plt.figure()
+        Ax  = Fig.add_axes([ 0.1, 0.1, 0.7, 0.8 ])
+        
+        Xaxis = plu.AxisConfig('x', Xlims, Xlabel)
+        Xaxis.fontsize = 12
+        Xaxis.scale = 'log'
+        
+        Yaxis = plu.AxisConfig('y', Ylims, Ylabel)
+        Yaxis.fontsize = 12
+        Yaxis.scale = 'log'
+        
+        Ptitle = plu.TitleConfig('', Pname)
+        Ptitle.fontsize = 24
+        
+        Legend = plu.LegendConfig(SimLegText, 'upper left')
+        Legend.bbox = [ 1.02, 1.0 ]
+        Legend.fontsize = 9
+        Legend.ncol = 1
+        
+        plu.PlotLine(Ax, PER, VARS.transpose(), Ptitle, Xaxis, Yaxis, Legend, SimColors)
+        
+        
+        OutFile = "{0:s}/{1:s}PowerSpectrum{2:s}.png".format(Pdir, Pfprefix, Oname)
+        print("Writing: {0:s}".format(OutFile))
+        Fig.savefig(OutFile)
+        plt.close()
+
+        print("")
+        
